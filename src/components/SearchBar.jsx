c8f4599fface45f32fa7453acd9083ca67c08171
Qu4k3r <neves0007@gmail.com>
2021-02-24 16:05:01 -0300

    Requisitos 3 & 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ class SearchBar extends Component {
   render() {
     const { searchText,
       onSearchTextChange,
-      bookmarkedOmly,
+      bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
       onSelectedGenreChange,
@@ -13,7 +13,26 @@ class SearchBar extends Component {
     } = this.props;
 
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="movieSearch">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="movieGenreSearch">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+      </form>
     );
   }
 }
