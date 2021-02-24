e28e5f4f4d5e13086fb21103d61a74b323587db3
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-24 16:27:36 -0300

    Ex. 5 - select options of gender

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,34 +3,41 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      nSelectedGenreChange
-    } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="text-input">
           Inclui o texto:
           <input
-            id="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
           />
         </label>
+
         <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
           Mostrar somente favoritos
           <input
             type="checkbox"
-            id="checkbox-input"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
+        </label>
+
+        <label data-testid="select-input-label" htmlFor="select-input">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
@@ -38,3 +45,17 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+// const genders = [
+//   { value: '', name: 'Todos' },
+//   { value: 'action', name: 'Ação' },
+//   { value: 'comedy', name: 'Comédia' },
+//   { value: 'thriller', name: 'Suspense' },
+// ];
+// {genders.map((gender) => (<option
+//   key={ gender.value }
+//   value={ gender.value }
+//   data-testid="select-option"
+// >
+//   { gender.name }
+// </option>))}
