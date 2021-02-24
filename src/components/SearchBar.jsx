965e3cc3ea9a4d1ac18eed874c0ca452db65d4e3
Nathália <nathaliaveneziano@icloud.com>
2021-02-24 18:46:40 -0300

    Preparando o projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return(
+      <p>SearchBar</p>
+    )
+  }
+}
+
+export default SearchBar;
