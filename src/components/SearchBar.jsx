624934e65930a34575c10ac7f7d29101871223f4
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-24 16:03:34 -0300

    Req. 2 - Render a form in <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  /* constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      onSearchTextChange: '',
+
+    }
+  } */
+  render() {
+    return (
+      <div>
+        <form data-testid="search-bar-form" />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
