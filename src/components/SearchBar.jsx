a70dcab588b5c9a21ee946cbde524c89597d35d3
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-02-24 15:30:12 -0400

    Requisito 3 e 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,17 +2,49 @@ import React, { Component } from 'react';
 import { string, func, bool } from 'prop-types';
 
 class SearchBar extends Component {
-  render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+  constructor() {
+    super();
+
+    this.inputText = this.inputText.bind(this);
+    this.inputCheckbox = this.inputCheckbox.bind(this);
+  }
+
+  inputText() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form data-testid="search-bar-form" />
+      <label data-testid="text-input-label" htmlFor="text-input">
+        Inclui o texto:
+        <input
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+
+  inputCheckbox() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+        Mostrar somente favoritos:
+        <input
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        { this.inputText() }
+        { this.inputCheckbox() }
+      </form>
     );
   }
 }
