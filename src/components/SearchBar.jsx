debde5a470d6172dbd1c8d7a8d3e5055e85dd1f1
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-24 16:29:36 -0300

    Req. 3 - Render a text input in forms on <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
       searchText: '',
-      onSearchTextChange: '',
+      onSearchTextChange: ''
+    };
 
-    }
-  } */
+  }
+
+  handleChange({ event }) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <div>
-        <form data-testid="search-bar-form" />
+        <form data-testid="search-bar-form">
+          <label htmlFor="text-input-label" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
+        </form>
       </div>
     );
   }
