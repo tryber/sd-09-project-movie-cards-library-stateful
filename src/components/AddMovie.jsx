f2c9efa50129f2f792d002836cff3a25f628c814
Felipe Cardoso <felipedclc@gmail.com>
2021-02-24 19:44:02 -0300

    comitao bomba para salvar o estado do projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,84 @@
 // implement AddMovie component here
+
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">
+          Título
+                   <input
+            name="title"
+            type="text"
+            value={this.state.title}
+            data-testid="title-input"
+            onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+                   <input
+            name="subtitle"
+            type="text"
+            value={this.state.subtitle}
+            data-testid="subtitle-input"
+            onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="image-input-label">
+          Imagem
+                   <input
+            name="imagePath"
+            type="text"
+            value={this.state.imagePath}
+            data-testid="image-input"
+            onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="storyline-input-label">
+          Sinopse
+                   <textarea
+            name="storyline"
+            value={this.state.storyline}
+            data-testid="storyline-input"
+            onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="rating-input-label">
+          Avaliação
+                   <input
+            name="rating"
+            type="number"
+            value={this.state.rating}
+            data-testid="rating-input"
+            onChange={this.handleChange}
+          />
+        </label>
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,64 @@
 // implement AddMovie component here
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="">
+          Inclui o texto:
+                    <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+            <input
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+              <select
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">
+              Todos
+                  </option>
+            <option data-testid="select-option" value="action">
+              Ação
+                  </option>
+            <option data-testid="select-option" value="comedy">
+              Comédia
+                  </option>
+            <option data-testid="select-option" value="thriller">
+              Suspense
+                  </option>
+          </select>
+        </label>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
