17aaeabe083230c3b20877262027dcc0e293a9ba
Daniela Muller <danielamuller20@gmail.com>
2021-02-24 15:54:46 -0300

    SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
-
   render() {
-    const { 
+    const {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
