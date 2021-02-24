9f56f066e2584d7dc3bbe0a0082ba8d12afb1ac1
Renato Feitosa <pythaqua@gmail.com>
2021-02-24 14:24:49 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-
+// Just a comment to make the commit
 function App() {
   return (
     <div className="App">
