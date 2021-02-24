2ebe12f5feab2395fe9c0f7323f3e9f7806d8947
ViniciusChagas <viniciushrcs@gmail.com>
2021-02-24 18:17:54 -0300

    requisito 8, renderizar um input para obter o título do novo filme

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,13 +12,30 @@ class AddMovie extends React.Component {
             rating: 0,
             genre: 'action',
         }
+        this.changeHandler = this.changeHandler.bind(this);
     }
+
+    changeHandler({ target }){
+        const { name, value } = target
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
         const { onClick } = this.props
         return (
             <div>
                 <form data-testid="add-movie-form">
-
+                    <label data-testid="title-input-label">
+                        Título
+                        <input type="text"
+                        name="title"
+                        value={this.state.title} 
+                        data-testid="title-input"
+                        onChange={this.changeHandler}
+                        />
+                    </label>
                 </form>
             </div>
         )
