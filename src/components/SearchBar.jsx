61e40d74cda2e4ee760a5d696b3c103b29e9e45a
MarcioFujita <hanker78@gmail.com>
2021-02-24 17:43:39 -0300

    SearchBar V6

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,31 +15,37 @@ class SearchBar extends React.Component {
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="searchInput">
             Inclui o texto:
-          <input type="text" 
-          value={ searchText } 
-          onChange={ onSearchTextChange } data-testid="text-input" 
-          id="searchInput"
+          <input
+            type="text" 
+            value={ searchText } 
+            onChange={ onSearchTextChange }
+            data-testid="text-input" 
+            id="searchInput"
           />
         </label>
 
         <label
         data-testid="checkbox-input-label"
-        htmlFor="bookmarkInput">
+        htmlFor="bookmarkInput"
+        >
             Mostrar somente favoritos
-          <input 
-          type="checkbox"
-          checked={ bookmarkedOnly }
-          onChange={ onBookmarkedChange }data-testid="checkbox-input"
-          id="bookmarkInput" />
+          <input
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange}
+            data-testid="checkbox-input"
+            id="bookmarkInput"
+          />
         </label>
 
         <label data-testid="select-input-label" htmlFor="genreSelect">
             Filtrar por gênero
-          <select 
-          value={ selectedGenre }
-          onChange={ onSelectedGenreChange }
-          data-testid="select-input"
-          id="genreSelect">
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+            id="genreSelect"
+          >
             <option value="" data-testid="select-option"> Todos </option>
             <option value="action" data-testid="select-option"> Ação </option>
             <option value="comedy" data-testid="select-option"> Comédia </option>
