0aeba8b3b197ff661c820717020c9f7d2f37f33f
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-24 19:46:40 -0300

    Ex. 15/16 - render MovieLibrary component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,22 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default MovieLibrary;
