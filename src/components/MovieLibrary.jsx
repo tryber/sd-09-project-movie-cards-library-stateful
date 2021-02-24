6e6b1153f10fb76d286faf2902439376f8d98142
LucioOSilva <lucioosilva00@gmail.com>
2021-02-24 14:25:27 -0300

    iniciando o projeto - movies-card-stateful

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import data from '../data';
+
+class MovieLibrary extends Component {
+  // constructor() {
+  //   super()
+
+  // }
+
+  render() {
+    const movies = data;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = { movies: PropTypes.array }.isRequired;
+
+export default MovieLibrary;
