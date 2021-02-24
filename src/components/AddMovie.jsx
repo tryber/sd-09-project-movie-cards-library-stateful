bbb4853f0acebf2f85c698c099c28b3d19ee2e75
Nathália <nathaliaveneziano@icloud.com>
2021-02-24 20:48:10 -0300

    Requisito 08 - Renderize um input do tipo texto dentro do formulário em <AddMovie /> para obter o título do novo filme

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,26 +1,50 @@
 // implement AddMovie component here
 import React, { Component } from "react";
 import PropTypes, { func } from "prop-types";
+import { title } from "process";
 
 class AddMovie extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
       rating: 0,
-      genre: 'action'
+      genre: "action"
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value, dataset } = event.target;
+    const type = dataset.testid.split('-')[0];
+    
+    this.setState({
+      [type]: value
+    });
   }
 
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+
     return (
       <fieldset>
         <legend>Adicionar Filme</legend>
-        <form data-testid="add-movie-form"></form>
+        <form data-testid="add-movie-form">
+          <div>
+            <label data-testid="title-input-label">Título</label>
+            <input
+              type="text"
+              value={title}
+              data-testid="title-input"
+              onChange={this.handleChange}
+            />
+          </div>
+        </form>
       </fieldset>
     );
   }
