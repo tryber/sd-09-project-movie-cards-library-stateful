3161bfc19d4e58973a02facd96b48df77b6e6a19
Enio Nicael <enionicael@gmail.com>
2021-02-24 17:53:17 -0300

    Requisito 4 adiciona e renderiza um checkbox na searchBar.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,28 @@ class SearchBar extends Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-
+        <label htmlFor="search-text" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            name="search-text"
+            id="search-text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="bookmark" data-testid="text-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={bookmarkedOnly}
+            id="bookmark"
+            value={searchText}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
