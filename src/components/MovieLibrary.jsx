92c6ddc6c7690866e27e192954cc2d7f2661eecb
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-02-24 17:22:44 -0300

    fixing identation

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -12,90 +12,87 @@ class MovieLibrary extends Component {
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
     this.AddNewMovie = this.AddNewMovie.bind(this);
-        
+
     const { movies } = this.props;
-      this.state = {
-        searchText: '',
-        bookmarkedOnly: false,
-        selectedGenre: '',
-        movies,
-            
-  };
-}
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
 
-    onSearchTextChange({ target }) {
-        const { name, value } = target
-    
-        this.setState({
-          [name]: value
-        })
-      }
-    
-      onBookmarkedChange({ target }) {
-        const { name } = target
-        const value = target.type === 'checkbox' ? target.checked : target.value
-        this.setState({
-            [name]: value
-        })
-      }
-    
-      onSelectedGenreChange({ target }) {
-        const { name, value } = target
-        console.log(target.value)
-        this.setState({
-          [name]: value
-        })
-      }
+    };
+  }
 
-      AddNewMovie (newMovie) {
-        this.setState((prevState) => ({
-            movies:[...prevState.movies, newMovie],
-        }))
+  onSearchTextChange({ target }) {
+    const { name, value } = target
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    const { name } = target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    this.setState({
+      [name]: value,
+      });
+    }
+    
+  onSelectedGenreChange({ target }) {
+    const { name, value } = target
+      this.setState({
+        [name]: value
+      });
     }
 
-    inputTextFilter (movies) {
-        const { searchText } = this.state;
-        const textInput = movies.filter(
-          (name) => name.title.toLowerCase().indexOf(searchText.toLowerCase()) >= 0
-          || name.subtitle.toLowerCase()
-            .indexOf(searchText.toLowerCase()) >= 0 || name.storyline
-              .toLowerCase()
-              .indexOf(searchText.toLowerCase()) >= 0,
-        );
-    
-        return textInput;
-      }
-    
-      filters () {
-        const { movies, bookmarkedOnly, selectedGenre } = this.state;
-    
-        if (bookmarkedOnly && selectedGenre) {
-          return this.inputTextFilter(movies).filter(
-            (name) => name.bookmarked === true && name.genre === selectedGenre,
-          );
-        }
-        if (bookmarkedOnly) {
-          return this.inputTextFilter(movies).filter(
-            (name) => name.bookmarked === true,
-          );
-        }
-        if (selectedGenre) {
-          return this.inputTextFilter(movies).filter(
-            (name) => name.genre === selectedGenre,
-          );
-        }
-        return this.inputTextFilter(movies);
-      }
+  AddNewMovie (newMovie) {
+    this.setState((prevState) => ({
+      movies:[...prevState.movies, newMovie],
+      }));
+    }
 
-    handleMovies(movies) {
-      const { searchText, bookmarkedOnly, selectedGenre } = this.state;
-      let filterMovie = movies
-        .filter((movie) => movie.genre.toLowerCase().includes(selectedGenre))
-        .filter((movie) => movie.title.toLowerCase().includes(searchText)
-          || movie.subtitle.toLowerCase().includes(searchText)
-          || movie.storyline.toLowerCase().includes(searchText));
+  inputTextFilter (movies) {
+    const { searchText } = this.state;
+    const textInput = movies.filter(
+      (name) => name.title.toLowerCase().indexOf(searchText.toLowerCase()) >= 0
+      || name.subtitle.toLowerCase()
+      .indexOf(searchText.toLowerCase()) >= 0 || name.storyline
+      .toLowerCase().indexOf(searchText.toLowerCase()) >= 0,
+        );
+      return textInput;
+  }
+    
+  filters () {
+    const { movies, bookmarkedOnly, selectedGenre } = this.state;
+    
+    if (bookmarkedOnly && selectedGenre) {
+      return this.inputTextFilter(movies).filter(
+      (name) => name.bookmarked === true && name.genre === selectedGenre,
+      );
+    }
+    if (bookmarkedOnly) {
+      return this.inputTextFilter(movies).filter(
+        (name) => name.bookmarked === true,
+      );
+    }
+    if (selectedGenre) {
+      return this.inputTextFilter(movies).filter(
+      (name) => name.genre === selectedGenre,
+      );
+    }
+      return this.inputTextFilter(movies);
+  }
+
+  handleMovies(movies) {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    let filterMovie = movies
+      .filter((movie) => movie.genre.toLowerCase().includes(selectedGenre))
+      .filter((movie) => movie.title.toLowerCase().includes(searchText)
+      || movie.subtitle.toLowerCase().includes(searchText)
+      || movie.storyline.toLowerCase().includes(searchText));
       filterMovie = !bookmarkedOnly
-        ? filterMovie : filterMovie.filter((movie) => movie.bookmarked);
+      ? filterMovie : filterMovie.filter((movie) => movie.bookmarked);
       return filterMovie;
     }
     
