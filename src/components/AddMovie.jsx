4b9165039dae3411bfbd618da4d3a4d0d528d586
marcusWittho <witthoeft.mcs@hotmail.com>
2021-02-24 18:08:30 -0300

    Resoluçao do requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    }
+  }
+
   render() {
     return (
-      <>
-      </>
+      <form data-testid="add-movie-form">
+
+      </form>
     );
   }
 }
