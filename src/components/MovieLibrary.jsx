f7474e303e1c69eefd19e560812fceb154411c53
Natalia Negreiros <nataliaknegres@gmail.com>
2021-02-24 19:43:16 -0300

    Iniciando o projeto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
