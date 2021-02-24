83d1fb8de00f66060fbd4c1c1210b1703d1fa188
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-24 18:29:14 -0300

    Ex. 12 - input

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,53 +4,115 @@ class AddMovie extends React.Component {
   constructor(props) {
     super(props);
 
-    // this.hendleChenge = this.hendleChenge.bind(this);
     this.state = {
-      subtitle: '',
       title: '',
+      subtitle: '',
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
+      genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.titleInput = this.titleInput.bind(this);
+    this.subtitleInput = this.subtitleInput.bind(this);
+    this.imageInput = this.imageInput.bind(this);
+    this.storylineInput = this.storylineInput.bind(this);
+    this.ratingInput = this.ratingInput.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  titleInput(title) {
+    return (
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          name="title"
+          type="text"
+          data-testid="title-input"
+          value={ title }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  subtitleInput(subtitle) {
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+        Subtítulo
+        <input
+          name="subtitle"
+          type="text"
+          data-testid="subtitle-input"
+          value={ subtitle }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  imageInput(imagePath) {
+    return (
+      <label data-testid="image-input-label" htmlFor="image-input">
+        Imagem
+        <input
+          name="imagePath"
+          type="text"
+          data-testid="image-input"
+          value={ imagePath }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  storylineInput(storyline) {
+    return (
+      <label data-testid="storyline-input-label" htmlFor="storyline-input">
+        Sinopse
+        <input
+          name="storyline"
+          type="textarea"
+          data-testid="storyline-input"
+          value={ storyline }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  ratingInput(rating) {
+    return (
+      <label data-testid="rating-input-label" htmlFor="rating-input">
+        Avaliação
+        <input
+          name="rating"
+          type="number"
+          data-testid="rating-input"
+          value={ rating }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
   }
 
   render() {
     const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title-input">
-          Título
-          <input
-            type="text"
-            data-testid="title-input"
-            // onChange={}
-          />
-        </label>
-        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
-          Subtítulo
-          <input
-            type="text"
-            data-testid="subtitle-input"
-            // onChange={}
-          />
-        </label>
-        <label data-testid="image-input-label" htmlFor="image-input">
-          Imagem
-          <input
-            type="text"
-            data-testid="image-input"
-            // onChange={}
-          />
-        </label>
-        <label data-testid="storyline-input-label" htmlFor="storyline-input">
-          Sinopse
-          <input
-            type="textarea"
-            data-testid="storyline-input"
-            // onChange={}
-          />
-        </label>
+        {this.titleInput(title)}
+        {this.subtitleInput(subtitle)}
+        {this.imageInput(imagePath)}
+        {this.storylineInput(storyline)}
+        {this.ratingInput(rating)}
       </form>
     );
   }
