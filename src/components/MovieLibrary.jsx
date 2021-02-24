5d409235cc117ad3b300ceb1d65499eac0936e17
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-02-24 16:31:07 -0300

    tentando fazer o searchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
+    const filters = this.state;
+    console.log(filters.searchText);
+    console.log(filters.selectedGenre);
+    console.log(filters.bookmarkedOnly);
     return (
       <div>
-        <SearchBar />
+        <h2> My awesome movie library </h2>
+        <SearchBar filters={ filters } />
+        <MovieList movies={ filters.movies } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieLibrary;
