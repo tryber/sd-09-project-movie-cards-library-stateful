0ffa8e260614e6e77000d2d1ac72a00b996322fc
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-02-24 11:26:51 -0300

    Commit Teste

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
 // implement AddMovie component here
+import {Component} from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (<h1>Teste</h1>)
+  }
+}
+
+export default SearchBar;
