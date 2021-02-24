471d368da231acca21cf62ab0bbaf531abc657ea
Enio Nicael <enionicael@gmail.com>
2021-02-24 19:26:18 -0300

    Requisito 8 renderiza um input text no form do componente AddMovie.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,13 +12,32 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+  }
+
+  handleTitleChange(event) {
+    const { value } = event.target;
+    this.setState({
+      title: value
+    })
   }
 
   render() {
     const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-
+        <div>
+          <label htmlFor="" data-testid="title-input-label">
+            Título
+            <input
+              type="text"
+              value={this.state.title}
+              data-testid="title-input"
+              onChange={this.handleTitleChange}
+            />
+          </label>
+        </div>
       </form>
     );
   }
