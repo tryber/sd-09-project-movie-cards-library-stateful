9c0c6c49f708ae8f7a7ee2a7957ee553759ee604
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-24 15:14:25 -0300

    Project: Movie Cards Library Stateful | Rafael Gerônimo

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,16 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>My awesome movie library</h2>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
