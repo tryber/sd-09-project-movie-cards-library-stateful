5560783a5511d6e339bcfa1bf6e62b14002771cf
marcusWittho <witthoeft.mcs@hotmail.com>
2021-02-24 17:31:43 -0300

    Resoluçao do requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,23 @@ class SearchBar extends React.Component {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            value={searchText}
+            type="text"
+            onChange={onSearchTextChange}
+            data-testid="text-input" />
+        </label>
 
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+          type="checkbox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+          data-testid="checkbox-input" />
+        </label>
       </form>
     );
   }
