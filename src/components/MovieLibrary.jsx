045475f8259f33969a3ac3baf036a408bce96757
Diogenes <diogenesfelipe.s@outlook.com>
2021-02-24 17:59:40 -0300

    feature/create component MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React from 'react';
+import { arrayOf, object } from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return ();
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: arrayOf(object),
+}
+
+export default MovieLibrary;
