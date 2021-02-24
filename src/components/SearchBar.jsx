73daab91664c0cd072f41f463a667869743d3865
Qu4k3r <neves0007@gmail.com>
2021-02-24 15:45:43 -0300

    Requisitos 1 & 2 | Comeco do projeto, Let\'s goo duude!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOmly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form" />
+    );
+  }
+}
+
+export default SearchBar;
