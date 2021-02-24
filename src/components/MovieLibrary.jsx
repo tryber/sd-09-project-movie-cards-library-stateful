2d5619eed63e2c9f609fa4239c66e99b346f1a60
LucioOSilva <lucioosilva00@gmail.com>
2021-02-24 14:32:40 -0300

    added SearchBar component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 import data from '../data';
 
 class MovieLibrary extends Component {
