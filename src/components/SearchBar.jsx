04ef7b0746523722fb2a2107a3a2f31e0ec1cb31
brenno-calado <brenno.calado@gmail.com>
2021-02-24 17:29:34 -0300

    skipped tests and & add SearchBar component and form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form className="form">
+        <fieldset>
+          <input type="text" />
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
