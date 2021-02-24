97cf5dd4d14bac1dd9d47928113975d67cf158f6
Khyrki <khyrkimitsunobi@gmail.com>
2021-02-24 16:21:18 -0300

    Add Requisito 1 e 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,31 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import movies from '../data';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre, } = this.props;
+
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar searchText={ searchText } onSearchTextChange={ this.onSearchTextChange } bookmarkedOnly={ bookmarkedOnly } onBookmarkedChange={ this.onBookmarkedChange } selectedGenre={ selectedGenre } onSelectedGenreChange={ this.onSelectedGenreChange } />
+        {/* <MovieList movies={ movies } />
+        <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <fieldset>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label"> 
+            Inclui o texto:
+            <input type="text" name="" id="" value={searchText} onChange={this.props.onSearchTextChange} data-testid="text-input" />
+          </label>
+        </form>
+      </fieldset>
+    );
+  }
+}
+
+export default SearchBar;
