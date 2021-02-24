f8ae0188a9ad283b1f7a65e56e44bede0c5280df
ViniciusChagas <viniciushrcs@gmail.com>
2021-02-24 17:43:23 -0300

    requisito 4, renderizar um input do tipo checkbox

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -21,6 +21,14 @@ class SearchBar extends React.Component {
                         data-testid="text-input"
                         />
                     </label>
+                    <label data-testid="checkbox-input-label">
+                        Mostrar somente favoritos
+                        <input type="checkbox"
+                        checked={bookmarkedOnly}
+                        onChange={onBookmarkedChange}
+                        data-testid="checkbox-input"
+                        />
+                    </label>
                 </form>
             </div>
         )
