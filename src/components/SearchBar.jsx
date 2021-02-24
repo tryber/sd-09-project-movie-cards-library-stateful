9007b5ca8366d456599d67b16b3ec1414631a365
Diogenes <diogenesfelipe.s@outlook.com>
2021-02-24 14:58:26 -0300

    Initial commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (<p>foi</p>);
+  }
+}
+
+export default SearchBar;
