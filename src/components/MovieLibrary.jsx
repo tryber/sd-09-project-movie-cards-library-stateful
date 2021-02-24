337337b32ec2daab3954c365dde5301cc0f5335d
santiago <felipesantiagolds@gmail.com>
2021-02-24 15:52:24 -0300

    Init commit

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement AddMovie component here
+import React from 'react';
+
+export default class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        movie library
+      </div>
+    );
+  }
+}
