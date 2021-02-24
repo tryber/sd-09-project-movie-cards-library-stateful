5977ee354d4ff346d969c483f7911b608be3d323
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-24 15:39:30 -0300

    Ex. 1/3 - Component SearchBar, props, form and input with label

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ class Header extends React.Component {
     return (
       <header className="movie-card-header">
         <h1 className="page-title">Movie Cards Library</h1>
-        
       </header>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,30 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      nSelectedGenreChange
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="searcText">
+          Inclui o texto:
+          <input
+            id="searcText"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
