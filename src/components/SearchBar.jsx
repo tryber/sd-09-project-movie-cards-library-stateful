83933dd8edeff4844279eece19f7f8e4db1a7abe
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-02-24 17:29:18 -0300

    Requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <p>SearchBar</p>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
