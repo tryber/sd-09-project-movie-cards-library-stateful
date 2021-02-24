a1b0bce3b545d424b93e104e8f02851616690dba
Thamirys <thamirysgoncalves.prog@gmail.com>
2021-02-24 18:12:12 -0300

    realizado requisito 06

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form action="">
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
