e559206ce56a414a08ee2738ab54632bba648df7
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-02-24 17:58:22 -0300

    fixing errors

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -40,43 +40,43 @@ class MovieLibrary extends Component {
     });
   }
 
-  AddNewMovie (newMovie) {
+  AddNewMovie(newMovie) {
     this.setState((prevState) => ({
-      movies:[...prevState.movies, newMovie],
+      movies: [...prevState.movies, newMovie],
     }));
   }
 
-  inputTextFilter (movies) {
+  inputTextFilter(movies) {
     const { searchText } = this.state;
     const textInput = movies.filter(
       (name) => name.title.toLowerCase().indexOf(searchText.toLowerCase()) >= 0
       || name.subtitle.toLowerCase()
-      .indexOf(searchText.toLowerCase()) >= 0 || name.storyline
-      .toLowerCase().indexOf(searchText.toLowerCase()) >= 0,
-        );
-      return textInput;
+        .indexOf(searchText.toLowerCase()) >= 0 || name.storyline
+        .toLowerCase().indexOf(searchText.toLowerCase()) >= 0,
+    );
+    return textInput;
   }
-    
-  filters () {
+
+  filters() {
     const { movies, bookmarkedOnly, selectedGenre } = this.state;
-    
+
     if (bookmarkedOnly && selectedGenre) {
       return this.inputTextFilter(movies).filter(
-      (name) => name.bookmarked === true && name.genre === selectedGenre,
-    );
-  }
+        (name) => name.bookmarked === true && name.genre === selectedGenre,
+      );
+   }
     if (bookmarkedOnly) {
       return this.inputTextFilter(movies).filter(
         (name) => name.bookmarked === true,
-    );
-  }
+      );
+   }
     if (selectedGenre) {
       return this.inputTextFilter(movies).filter(
-      (name) => name.genre === selectedGenre,
-    );
+        (name) => name.genre === selectedGenre,
+      );
+   }
+    return this.inputTextFilter(movies);
   }
-      return this.inputTextFilter(movies);
-}
 
   handleMovies(movies) {
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
@@ -85,11 +85,11 @@ class MovieLibrary extends Component {
       .filter((movie) => movie.title.toLowerCase().includes(searchText)
       || movie.subtitle.toLowerCase().includes(searchText)
       || movie.storyline.toLowerCase().includes(searchText));
-      filterMovie = !bookmarkedOnly
+    filterMovie = !bookmarkedOnly
       ? filterMovie : filterMovie.filter((movie) => movie.bookmarked);
-      return filterMovie;
-    }
-    
+    return filterMovie;
+  }
+
   render() {
     const { searchText, bookmarkedOnly, movies, selectedGenre } = this.state;
 
@@ -98,16 +98,16 @@ class MovieLibrary extends Component {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar 
-                  onSearchTextChange={this.onSearchTextChange}
-                  searchText={searchText}
-                  onBookmarkedChange={this.onBookmarkedChange}
-                  bookmarkedOnly={bookmarkedOnly}
-                  onSelectedGenreChange={this.onSearchTextChange}
-                  selectedGenre={selectedGenre}
-                />
+        <SearchBar
+          onSearchTextChange={ this.onSearchTextChange }
+          searchText={ searchText }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onSelectedGenreChange={ this.onSearchTextChange }
+          selectedGenre={ selectedGenre }
+        />
         <MovieList movies={ moviesFilter } />
-        <AddMovie onClick={this.AddNewMovie}/>
+        <AddMovie onClick={ this.AddNewMovie }/>
       </div>
     );
   }
