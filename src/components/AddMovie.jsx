f33e4775479c27725af9354b67c23224a44ad4c5
MarcioFujita <hanker78@gmail.com>
2021-02-24 20:34:45 -0300

    AddMovie V9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -51,7 +51,7 @@ class AddMovie extends React.Component {
           onChange={ this.handleChange }
           id="titleInput"
         />
-    </label>
+      </label>
     );
   }
 
@@ -125,20 +125,20 @@ class AddMovie extends React.Component {
   genreInput() {
     const { genre } = this.state;
     return (
-    <label data-testid="genre-input-label" htmlFor="genreInput">
-          Gênero
-      <select
-        data-testid="genre-input"
-        name="genre"
-        value={ genre }
-        onChange={ this.handleChange }
-        id="genreInput"
-      >
-      <option data-testid="genre-option" value="action">Ação</option>
-      <option data-testid="genre-option" value="comedy">Comédia</option>
-      <option data-testid="genre-option" value="thriller">Suspense</option>
-      </select>
-    </label>
+      <label data-testid="genre-input-label" htmlFor="genreInput">
+        Gênero
+        <select
+          data-testid="genre-input"
+          name="genre"
+          value={ genre }
+          onChange={ this.handleChange }
+          id="genreInput"
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
     );
   }
 
