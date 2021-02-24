b0a81ed6f4dc61ca73ea1476193e3843cf9dc0d7
lcgoandete <lcgoandete@hotmail.com>
2021-02-24 17:48:57 -0300

    requisito 7 Renderize um formulário dentro de `<AddMovie />`

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { func } from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+
+    };
+  }
+
   render() {
+    const { onClick } = this.props;
     return (
       <div>
-        AddMovie
+        <form data-testid="add-movie-form" />
       </div>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: func.isRequired,
+};
 
 export default AddMovie;
