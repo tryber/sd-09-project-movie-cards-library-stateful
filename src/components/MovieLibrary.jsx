0f840edfa4290c975fffbee5494d158d67b86feb
brenno-calado <brenno.calado@gmail.com>
2021-02-24 17:57:09 -0300

    fit searchbar, movielist and addmovie inside movielibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import movies from '../data';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
