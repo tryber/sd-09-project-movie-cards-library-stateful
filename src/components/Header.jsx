1233394a3c6285abfe04c6d36174068afb7881fa
saulokirchmaier <saulokirchmaier@gmail.com>
2021-02-24 14:35:46 -0300

    Frist :rocket: GoMovie

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ class Header extends React.Component {
     return (
       <header className="movie-card-header">
         <h1 className="page-title">Movie Cards Library</h1>
+        
       </header>
     );
   }
