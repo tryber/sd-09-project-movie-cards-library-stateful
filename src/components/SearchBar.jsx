9c26833b3c3025d175df249bed0ee783237c196b
Diogenes <diogenesfelipe.s@outlook.com>
2021-02-24 15:40:57 -0300

    feature/Component SearchBar complete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,75 @@
 // implement AddMovie component here
 import React from 'react';
+import { string, func, bool } from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    return (<p>foi</p>);
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="search">
+          Inclui o texto:
+          <input
+            type="text"
+            id="searchInput"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+
+        <label data-testid="checkbox-input-label" htmlFor="searchCheckBox">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="searchCheckBox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+
+        <label data-testid="select-input-label" htmlFor="searchSelect">
+          Filtrar por gênero
+          <select
+            id="searchSelect"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: string,
+  onSearchTextChange: func,
+  bookmarkedOnly: bool,
+  onBookmarkedChange: func,
+  selectedGenre: string,
+  onSelectedGenreChange: func,
+};
+
+SearchBar.defaultProps = {
+  searchText: '',
+  onSearchTextChange: () => {},
+  bookmarkedOnly: Boolean,
+  onBookmarkedChange: () => {},
+  selectedGenre: '',
+  onSelectedGenreChange: () => {},
+};
 
 export default SearchBar;
