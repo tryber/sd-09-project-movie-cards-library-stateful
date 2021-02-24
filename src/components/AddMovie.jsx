1c47fd3eda562c0b227a595d061637a272ba6980
Nathália <nathaliaveneziano@icloud.com>
2021-02-24 20:05:32 -0300

    Requisito 06 - Crie um componente chamado <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,28 @@
 // implement AddMovie component here
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PropTypes, { func } from "prop-types";
 
 class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    };
+  }
+
   render() {
-    return(
-      <p>AddMovie</p>
-    )
+    return <p>AddMovie</p>;
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func
+};
 
 export default AddMovie;
