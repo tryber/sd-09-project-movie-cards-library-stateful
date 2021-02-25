8f0e252f6aeb0283f93a8486e33dc27e771bc2c4
ggaldino95 <ggaldino95@gmail.com>
2021-02-25 01:18:34 -0300

    [Task 1] SearchBar implemented with necessary props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return 0;
+  }
+}
+
+export default SearchBar;
