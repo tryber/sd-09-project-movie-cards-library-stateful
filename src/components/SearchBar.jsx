97f799919d2b1b42b89d3b691c4ecf6ed854ad52
Carine Becker <caarine.m@gmail.com>
2021-02-25 14:27:55 -0300

    requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,6 +24,17 @@ class SearchBar extends Component {
             data-testid="text-input"
           />
         </label>
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+          Mostrar somente favoritos
+          <input
+            id="checkbox-input"
+            type="checkbox"
+            value={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            checked
+          />
+        </label>
       </form>
     );
   }
