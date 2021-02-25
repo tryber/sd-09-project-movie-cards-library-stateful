63b02505dac07e6e0f63938edccf0d27787e3ec6
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-25 10:16:38 -0300

    6 - Crie um componente chamado <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,19 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.props;
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default AddMovie;
