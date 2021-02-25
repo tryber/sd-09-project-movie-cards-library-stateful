1c4d715cddacca3cbbd545424f00921da9970a23
Natalia Negreiros <nataliaknegres@gmail.com>
2021-02-24 22:00:37 -0300

    add requirements 1-5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,51 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input-id" data-testid="text-input-label">
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+          Inclui o texto:
+        </label>
+        <label htmlFor="checkbox-input-id" data-testid="checkbox-input-label">
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+          Mostrar somente favoritos
+        </label>
+        <label htmlFor="select-input-id" data-testid="select-input-label">
+          <select
+            value={ selectedGenre }
+            data-testid="select-input"
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+          Filtrar por gênero
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
