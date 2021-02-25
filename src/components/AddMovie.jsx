6f6be85fc76b439a95619bdd87bca62bb699b4fa
ggaldino95 <ggaldino95@gmail.com>
2021-02-25 02:32:06 -0300

    [Task 9] Movie Subtitle input implemented inside the AddMovie form

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,6 +5,7 @@ class AddMovie extends React.Component {
   constructor() {
     super();
     this.handleTitleInput = this.handleTitleInput.bind(this);
+    this.handleSubtitleInput = this.handleSubtitleInput.bind(this);
 
     this.state = {
       subtitle: '',
@@ -18,6 +19,10 @@ class AddMovie extends React.Component {
 
   handleTitleInput(event) {
     this.setState({ title: event.target.value });
+  }
+
+  handleSubtitleInput(event) {
+    this.setState({ subtitle: event.target.value });
   }
 
   render() {
@@ -34,6 +39,16 @@ class AddMovie extends React.Component {
             value={ title }
             onChange={ this.handleTitleInput }
             data-testid="title-input"
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            id="subtitle-input"
+            value={ subtitle }
+            onChange={ this.handleSubtitleInput }
+            data-testid="subtitle-input"
           />
         </label>
       </form>
