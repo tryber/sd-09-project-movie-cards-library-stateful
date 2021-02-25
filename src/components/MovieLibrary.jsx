38145fea368822508fd7463f246f7e190352a47e
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-02-25 16:57:37 -0300

    Add SearchBar component to MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,33 @@
-// implement AddMovie component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: 'false',
+      selectedGenre: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
