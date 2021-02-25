13f798df1a5db8eb681370a57f03dffb64c032de
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-02-24 23:19:05 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,9 +5,16 @@ class AddMovie extends Component {
     return (
       <div>
         <p>AddMovie</p>
-        <form
-          data-testid="add-movie-form"
-        />
+        <form data-testid="add-movie-form">
+          <label htmlFor="title-input" data-testid="title-input-label">
+            Título
+            <input
+              data-testid="title-input"
+              title="title-input"
+              type="text"
+            />
+          </label>
+        </form>
       </div>
     );
   }
