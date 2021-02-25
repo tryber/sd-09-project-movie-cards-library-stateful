48b9ecfc5898f10afd9e9f4bb9898f0216b00430
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-02-25 02:34:23 -0300

    Requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
   render() {
     return (
       <div>
         <p>Movie Library</p>
+        <SearchBar />
       </div>
     );
   }
