b687218515dcf3f057b85a5bc8ba9dc9ea44373e
Uibirá <usbarreto@gmail.com>
2021-02-25 18:28:48 -0300

    Req.5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,8 +8,8 @@ class SearchBar extends Component {
   render() {
     const { searchText, onSearchTextChange } = this.props;
     const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    const { selectedGenre, onSelectedGenreChange } = this.props;
     return (
-    // <div>Make it so</div>
       <div>
         { searchText }
         <form data-testid="search-bar-form">
@@ -31,6 +31,19 @@ class SearchBar extends Component {
             />
             Mostrar somente favoritos
           </label>
+          <label data-testid="select-input-label" htmlFor="select-input">
+            Filtrar por gênero
+            <select
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
@@ -42,8 +55,8 @@ SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
-//   selectedGenre: PropTypes.string.isRequired,
-//   onSelectedGenreChange: PropTypes.string.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.string.isRequired,
 };
 
 export default SearchBar;
