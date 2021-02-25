8f39901a01c2af0be259cc29f1009331578ba14d
edison-ps <edison@osite.com.br>
2021-02-25 16:02:08 -0300

    Estrutura SeachBar V1.00

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,42 @@
-// implement AddMovie component here
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  render() {
+    return (
+    <div>
+      <form>
+        <div>
+          <label>Inclui o texto</label>
+        </div>
+        <div>
+          <input type="text" name="searchText" />
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" name="bookmarkedOnly" />
+            Mostrar somente favoritos
+          </label>
+          <div>
+            <label>
+              Filtrar por gênero
+            </label>
+          </div>
+          <div>
+            <select>
+              <option value="action">Ação</option>
+              <option value="comedy">Comédia</option>
+              <option value="fantasy">Fantasia</option>
+            </select>
+          </div>
+          
+         
+        </div>
+        
+      </form> 
+    </div>
+    );
+  }
+
+}
+
+export default SearchBar;
