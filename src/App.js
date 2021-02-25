3352b16501efd381a34c3bfa81ad090bca140ff2
Christopher <chris.tryber@gmail.com>
2021-02-25 15:33:07 -0300

    Adicionei os inputs e labels do titulo e subtitulo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,54 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.retryStateTitle = this.retryStateTitle.bind(this);
+  }
+
+  retryStateTitle(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { title, subtitle } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="label-title" data-testid="title-input-label">
+          Título
+          <input
+            name="title"
+            id="label-title"
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.retryStateTitle }
+          />
+        </label>
+        <label htmlFor="label-subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            name="subtitle"
+            id="label-subtitle"
+            type="text"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.retryStateTitle }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
