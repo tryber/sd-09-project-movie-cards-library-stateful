c391de54d4e1ca3cb85a1d6dcb39a6d6889c163c
Luciano_Lodi <lrlodi@gmail.com>
2021-02-25 20:02:06 -0300

    Adiciona o Botao em AddMovie

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import data from '../data';
+// import data from '../data';
 // import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: { data },
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   // this.state = {
+  //   //   searchText: '',
+  //   //   bookmarkedOnly: false,
+  //   //   selectedGenre: '',
+  //   //   movies: { data },
+  //   // };
+  // }
 
   render() {
     // const { movies } = this.props;
