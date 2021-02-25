7ef8ed6e2ea6db22f3cf09e606e46f892a5b20dc
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-02-25 02:16:00 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <p>Movie Library</p>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
