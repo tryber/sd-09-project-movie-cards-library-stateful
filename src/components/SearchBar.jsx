75733fb7d5dcc5e200d74b675b2e554d11d41352
RodrigoNeoEx <draja.acc@gmail.com>
2021-02-25 14:00:51 -0300

    climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,20 +13,23 @@ const SearchBar = () => (
   <form data-testid="search-bar-form">
     <label htmlFor="addText" data-testid="text-input-label">
       Inclui o texto:
-      <input
-        id="addText" type="text" value={ searchText } onChange={ onSearchTextChange }
-        data-testid="text-input"/>
+      <input type="text"
+        id="addText" value={ searchText } onChange={ onSearchTextChange }
+        data-testid="text-input"
+      />
     </label>
     <label htmlFor="Fav" data-testid="checkbox-input-label">
       Mostrar somente favoritos
-      <input
-        id="Fav" type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange }
-        data-testid="checkbox-input"/>
+      <input type="checkbox"
+        id="Fav" checked={ bookmarkedOnly } onChange={ onBookmarkedChange }
+        data-testid="checkbox-input"
+      />
     </label>
     <label htmlFor="filterGenre"  data-testid="select-input-label">
       Filtrar por gênero
       <select data-testid="select-input"
-          id="filterGenre" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+      id="filterGenre" value={ selectedGenre } onChange={ onSelectedGenreChange }
+      >
         <option value="" data-testid="select-option">Todos</option>
         <option value="action" data-testid="select-option">Ação</option>
         <option value="comedy" data-testid="select-option">Comédia</option>
