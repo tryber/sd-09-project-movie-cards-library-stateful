4ef94f3c92b792193c71560785536508bcd9f1d3
Carine Becker <caarine.m@gmail.com>
2021-02-25 14:34:04 -0300

    requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -35,6 +35,20 @@ class SearchBar extends Component {
             checked
           />
         </label>
+        <label data-testid="select-input-label" htmlFor="select-input">
+          Filtrar por gênero
+          <select
+            id="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
