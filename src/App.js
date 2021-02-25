fa3649d9e61e0a08fb3fb4a67331f08ad1e68842
RodrigoNeoEx <draja.acc@gmail.com>
2021-02-25 10:21:01 -0300

    changed app strutucture

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie'
-import movies from './data';
-import MovieList from './components/MovieList';
+import MovieLibrary from './components/MovieLibrary'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
-      <MovieList movies={ movies } />
-
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,2 +1,19 @@
 // implement AddMovie component here
+import React from 'react'
+import SearchBar from './SearchBar'
+import AddMovie from './AddMovie'
+import movies from '../data';
+import MovieList from './MovieList'
 
+
+
+
+const MovieLibrary = () => (
+  <div>
+    <SearchBar />
+    <AddMovie />
+    <MovieList movies={ movies } />
+  </div>
+)
+
+export default MovieLibrary;
