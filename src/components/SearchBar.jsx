1139b43199f8a5a1252fc53c55c8ff4973ef6d86
cleber-teixeira <cleberlt@gmail.com>
2021-02-25 18:23:21 -0300

    Requisito 01 - Criar um componente chamado "<SearchBar />". :white_check_mark:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,31 @@
 // implement AddMovie component here
+// import { render } from '@testing-library/react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    // const {
+    //   searchText,
+    //   onSearchTextChange,
+    //   bookmarkedOnly,
+    //   onBookmarkedChange,
+    //   selectedGenre,
+    //   onSelectedGenreChange,
+    // } = this.props;
+    return (<div> </div>);
+  }
+}
+
+SearchBar.propTypes = {
+  props: PropTypes.shape({
+    searchText: PropTypes.string,
+    onSearchTextChange: PropTypes.func,
+    bookmarkedOnly: PropTypes.bool,
+    onBookmarkedChange: PropTypes.func,
+    selectedGenre: PropTypes.string,
+    onSelectedGenreChange: PropTypes.func,
+  }).isRequired,
+};
+
+export default SearchBar;
