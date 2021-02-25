52a6c0dc3499bb56b0794b28cdbb8f12f06d9320
Uibirá <usbarreto@gmail.com>
2021-02-25 18:57:57 -0300

    AddMovies: inicio

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,21 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    const onSearchTextChange = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="text-input">
+          Título
+          <input
+            onChange={ onSearchTextChange }
+            data-testid="title-input"
+            title=""
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
