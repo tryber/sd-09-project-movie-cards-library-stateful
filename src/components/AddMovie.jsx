e74b30b53d523ee2c63f1308cf4c0ed750abf966
Nathália <nathaliaveneziano@icloud.com>
2021-02-24 21:22:33 -0300

    Requisito 13 - Renderize um select do formulário em <AddMovie /> para selecionar o gênero do novo filme

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -85,6 +85,13 @@ class AddMovie extends Component {
               max="5"
             />
           </div>
+          <div><label data-testid="genre-input-label">Gênero</label>
+            <select value={genre} data-testid="genre-input" onChange={this.handleChange}>
+              <option data-testid="genre-option" value="action">Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
+            </select>
+          </div>
         </form>
       </fieldset>
     );
