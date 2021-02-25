15b4e39c542955516aa0a69d1acbad8b57289b63
Pablo Fernandes <pabloprates@gmail.com>
2021-02-25 14:07:35 -0300

    Requirements Six, Seven and Eight

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,45 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    subtitle: '',
+    title: '',
+    imagePath: '',
+    storyline: '',
+    rating: 0,
+    genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState = {
+      [name]: value,
+    };
+  }
+
+  render() {
+    // const { onClick } = this.props;
+    const { title } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="inputTitle">
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+            name="title"
+            id="inputTitle"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
