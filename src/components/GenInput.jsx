417bff5a216c062c1ea3af00a31926399a8938c0
Luciano_Lodi <lrlodi@gmail.com>
2021-02-25 18:26:28 -0300

    Incluindo componente GenInput

---
+++ src/components/GenInput.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class GenInput extends React.Component {
+  render() {
+    const { name, id, type, dtId, value, onChange, labelId, labelName } = this.props;
+    return (
+      <label htmlFor={ labelId } data-testid={ labelId }>
+        { labelName }
+        <input
+          name={ name }
+          id={ id }
+          type={ type }
+          data-testid={ dtId }
+          value={ value }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+GenInput.propTypes = ({
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  dtId: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  labelId: PropTypes.string.isRequired,
+  labelName: PropTypes.string.isRequired,
+});
+
+export default GenInput;
