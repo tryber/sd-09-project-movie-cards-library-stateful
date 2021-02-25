0222e9bfc234ea60109ac05bae707f11ed36bd5b
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-02-24 20:28:26 -0400

    Requisito 15 e 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,22 @@
-// começarei do zero essa b*s*t*
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default MovieLibrary;
