f3ce969ee227c3f58e95c17a1bb7806c2c885372
Pablo Fernandes <pabloprates@gmail.com>
2021-02-25 13:20:07 -0300

    Requirement Four

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -19,12 +19,21 @@ class SearchBar extends Component {
           <input
             id="tipedText"
             type="text"
-            data-testid="text-input-label"
+            data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
           />
         </label>
-
+        <label data-testid="checkbox-input-label" htmlFor="favSelect">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            id="favSelect"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+          />
+        </label>
       </form>
     );
   }
