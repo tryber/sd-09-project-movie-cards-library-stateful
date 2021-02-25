b5598d8df05705d59ae66ef12c36efa4e77a187e
Caio <caioqs.info@gmail.com>
2021-02-24 21:39:42 -0300

    Feito component SearchBar versao final / corriindo ESlist

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -11,7 +11,7 @@ class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ this.props.movies } />
+        <MovieList />
         <AddMovie />
       </div>
     );
