960ccf1314c15b09a614b07f8ff0482df4b2f205
ggaldino95 <ggaldino95@gmail.com>
2021-02-25 02:29:35 -0300

    [Task 8] Movie Title input implemented inside the AddMovie form

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 class AddMovie extends React.Component {
   constructor() {
     super();
+    this.handleTitleInput = this.handleTitleInput.bind(this);
 
     this.state = {
       subtitle: '',
@@ -15,12 +16,27 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleTitleInput(event) {
+    this.setState({ title: event.target.value });
+  }
+
   render() {
     const { onClick } = this.props;
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
 
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            id="title-input"
+            value={ title }
+            onChange={ this.handleTitleInput }
+            data-testid="title-input"
+          />
+        </label>
+      </form>
     );
   }
 }
