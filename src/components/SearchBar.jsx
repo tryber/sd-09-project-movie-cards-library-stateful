13b7355c15b58e809bdd372e9346f982c38e9218
RodrigoNeoEx <draja.acc@gmail.com>
2021-02-25 14:46:50 -0300

    climate of searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,13 +6,13 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
   render() {
     const {
-    searchText,
-    onSearchTextChange,
-    bookmarkedOnly,
-    onBookmarkedChange,
-    selectedGenre,
-    onSelectedGenreChange,
-  } = this.props;
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <div>
         <form action="" data-testid="search-bar-form">
@@ -53,13 +53,13 @@ class SearchBar extends React.Component {
   }
 }
 
-  SearchBar.propTypes = {
-    searchText: PropTypes.string.isRequired,
-    onSearchTextChange: PropTypes.func.isRequired,
-    bookmarkedOnly: PropTypes.bool.isRequired,
-    onBookmarkedChange: PropTypes.func.isRequired,
-    selectedGenre: PropTypes.string.isRequired,
-    onSelectedGenreChange: PropTypes.func.isRequired,
-  };
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
