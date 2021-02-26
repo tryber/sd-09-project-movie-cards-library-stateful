44c6154f7e4d53c9e7807716af5e88592c99dc11
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-26 16:35:40 -0300

    Ajuste lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 
 function App() {
   return (
