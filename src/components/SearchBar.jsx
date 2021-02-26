3c070178ac15e2616323ec21be6be8ab3fa93b13
danielfasanaro <danielfasanaro@gmail.com>
2021-02-26 17:05:18 -0300

    3rd Requiriment: add an input on SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -12,9 +13,15 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
+        <label htmlFor="searchText" data-testid="text-input-label">Inclui o texto:</label>
+        <input type="text" id="searchText" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+
+};
 
 export default SearchBar;
