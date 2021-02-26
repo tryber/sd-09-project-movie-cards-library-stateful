5a491611d5bcb181132b348f6aa464afda0e87e2
Bernardo Smith <bernardostech@gmail.com>
2021-02-26 13:34:54 -0500

    Comecando o desafio

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, {Components} from 'react';
+
+class SearchBar extends Components() {
+  render() {
+  const {searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label">
+            Inclui o texto"
+            <input data-testid="text-input" type="text" onChange={ onSearchTextChange } value={ searchText } />
+          </label>
+          <label data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            
+          </label>  
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
