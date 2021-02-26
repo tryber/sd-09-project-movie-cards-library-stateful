c145ad6ebf74ff7620d2785114d1cb5634afdd14
ggaldino95 <ggaldino95@gmail.com>
2021-02-25 22:09:36 -0300

    [Task 16] MovieLibrary inital state successfully defined

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
 import AddMovie from './components/AddMovie';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
+      <MovieLibrary movies={ data } />
       <AddMovie />
     </div>
   );
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
-    return 0;
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+
+    return (
+      <main>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ 0 }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ 0 }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ 0 }
+        />
+        <MovieList movies={ movies } />
+      </main>
+    );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
 
 export default MovieLibrary;
