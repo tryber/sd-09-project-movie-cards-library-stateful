6c33edf6b07d42b0fa95ec279d0bc81d6bc3107c
Matheus Martino <matheus.martino97@gmail.com>
2021-02-26 15:55:01 -0300

    Fixing lint issues.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,60 +2,81 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
-  render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+  constructor(props) {
+    super(props);
+
+    this.searchTextElement = this.searchTextElement.bind(this);
+    this.bookmarkedOnlyElement = this.bookmarkedOnlyElement.bind(this);
+    this.selectedGenreElement = this.selectedGenreElement.bind(this);
+  }
+  searchTextElement() {
+    const { searchText, onSearchTextChange } = this.props;
 
     return (
+      <label htmlFor="test-input" data-testid="text-input-label">
+        Inclui o texto
+        <input
+          type="text"
+          id="text-input"
+          data-testid="text-input"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+      </label>
+    );
+  }
+
+  bookmarkedOnlyElement() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+
+    return (
+      <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          id="checkbox-input"
+          data-testid="checkbox-input"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+        />
+      </label>
+    );
+  }
+
+  selectedGenreElement() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+
+    return (
+      <label htmlFor="select-input" data-testid="select-input-label">
+        Filtrar por gênero
+        <select
+          id="select-input"
+          data-testid="select-input"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+        >
+          <option data-testid="select-option" value="">
+            Todos
+          </option>
+          <option data-testid="select-option" value="action">
+            Ação
+          </option>
+          <option data-testid="select-option" value="comedy">
+            Comédia
+          </option>
+          <option data-testid="select-option" value="thriller">
+            Suspense
+          </option>
+        </select>
+      </label>
+    );
+  }
+  render() {
+    return (
       <form action="" data-testid="search-bar-form">
-        <label htmlFor="test-input" data-testid="text-input-label">
-          Inclui o texto
-          <input
-            type="text"
-            id="text-input"
-            data-testid="text-input"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-          />
-        </label>
-        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input
-            type="checkbox"
-            id="checkbox-input"
-            data-testid="checkbox-input"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-          />
-        </label>
-        <label htmlFor="select-input" data-testid="select-input-label">
-          Filtrar por gênero
-          <select
-            id="select-input"
-            data-testid="select-input"
-            value={ selectedGenre }
-            onChange={ onSelectedGenreChange }
-          >
-            <option data-testid="select-option" value="">
-              Todos
-            </option>
-            <option data-testid="select-option" value="action">
-              Ação
-            </option>
-            <option data-testid="select-option" value="comedy">
-              Comédia
-            </option>
-            <option data-testid="select-option" value="thriller">
-              Suspense
-            </option>
-          </select>
-        </label>
+        { this.searchTextElement() }
+        { this.bookmarkedOnlyElement() }
+        { this.selectedGenreElement() }
       </form>
     );
   }
