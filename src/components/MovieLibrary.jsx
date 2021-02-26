368bb40699c6e656a6e672e9c67e0b30c5edc85c
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-26 16:53:59 -0300

    SerchBar e MovieList chamada no component MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
 // implement AddMovie component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
