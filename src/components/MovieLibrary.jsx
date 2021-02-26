c8be5513c087fc3b2625acfca05e6cf5365d168a
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-02-26 14:58:43 -0300

    Correçao Eslint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -6,16 +6,12 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList />
         <AddMovie />
       </div>
     );
