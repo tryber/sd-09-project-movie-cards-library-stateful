e55de5ee8e67680ee8a02b5f2ab9acac9ca336d9
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-02-26 08:10:10 -0300

    requirement one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar searchText={String} onSearchTextChange={useCallback} bookmarkedOnly={Boolean} onBookmarkedChange={useCallback} selectedGenre={String} onSelectedGenreChange={useCallback}/>
     </div>
   );
 }
