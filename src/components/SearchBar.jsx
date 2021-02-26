e5229bbd9405166021f617db1fdd43f2e9e34bc2
Thiago Granville <thiagogranville@gmail.com>
2021-02-26 18:34:57 -0300

    Requisito 4: Renderiza checkbox em SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,13 +14,24 @@ class SearchBar extends React.Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">
+          <label data-testid="text-input-label" htmlFor="text-search">
             Inclui texto:
             <input
               type="text"
+              id="text-search"
               data-testid="text-input"
               value={ searchText }
               onChange={ onSearchTextChange }
+            />
+          </label>
+          <label data-testid="checkbox-input-label" htmlFor="checkbox-search">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              id="checkbox-search"
+              data-testid="checkbox-input"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
             />
           </label>
         </form>
