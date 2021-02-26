4bd3b914c114a0a2d407bb1a845126be58a3e6fa
Pedro <ped.alm2709@gmail.com>
2021-02-26 18:26:10 -0300

    cc addmovie 2

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React from 'react';
+
 class AddMovie extends React.Component {
   render() {
     this.state = {
@@ -9,7 +10,7 @@ class AddMovie extends React.Component {
       storyline: '',
       rating: 0,
       genre: 'action',
-    }
+    };
     return (
       <div>Componente</div>
     );
