841aad8e789fcc142b2965acb950eb50f999934f
Matheus Martino <matheus.martino97@gmail.com>
2021-02-26 15:40:42 -0300

    Fixing lint issues.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
@@ -13,41 +13,49 @@ class SearchBar extends Component {
     } = this.props;
 
     return (
-      <form action="" data-testid="search-bar-form">
-        <label htmlFor="test-input" data-testid="text-input-label">
+      <form action='' data-testid='search-bar-form'>
+        <label htmlFor='test-input' data-testid='text-input-label'>
           Inclui o texto
+          <input
+            type='text'
+            id='text-input'
+            data-testid='text-input'
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
         </label>
-        <input
-          type="text"
-          id="text-input"
-          data-testid="text-input"
-          value={ searchText }
-          onChange={ onSearchTextChange }
-        />
-        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+        <label htmlFor='checkbox-input' data-testid='checkbox-input-label'>
           Mostrar somente favoritos
-        </label>
         <input
-          type="checkbox"
-          id="checkbox-input"
-          data-testid="checkbox-input"
-          checked={ bookmarkedOnly }
-          onChange={ onBookmarkedChange }
+          type='checkbox'
+          id='checkbox-input'
+          data-testid='checkbox-input'
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
         />
-        <label htmlFor="select-input" data-testid="select-input-label">
-          Filtrar por gênero
         </label>
+        <label htmlFor='select-input' data-testid='select-input-label'>
+          Filtrar por gênero
         <select
-          id="select-input"
-          data-testid="select-input"
-          value={ selectedGenre }
-          onChange={ onSelectedGenreChange }
+          id='select-input'
+          data-testid='select-input'
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
         >
-          <option data-testid="select-option" value="">Todos</option>
-          <option data-testid="select-option" value="action">Ação</option>
-          <option data-testid="select-option" value="comedy">Comédia</option>
-          <option data-testid="select-option" value="thriller">Suspense</option>
+          <option data-testid='select-option' value=''>
+            Todos
+          </option>
+          <option data-testid='select-option' value='action'>
+            Ação
+          </option>
+          <option data-testid='select-option' value='comedy'>
+            Comédia
+          </option>
+          <option data-testid='select-option' value='thriller'>
+            Suspense
+          </option>
         </select>
+        </label>
       </form>
     );
   }
