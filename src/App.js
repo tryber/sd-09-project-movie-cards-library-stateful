661585186d4c1f44c9e9a97992b47864633f2d7b
Uibirá <usbarreto@gmail.com>
2021-02-26 22:40:48 -0300

    Tentativa e erro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Header from './components/Header';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText=""
+        onSearchTextChange={ () => useCallback }
+        bookmarkedOnly
+        onBookmarkedChange={ useCallback }
+        selectedGenre=""
+        onSelectedGenreChange={ () => 'useCallback' }
+      />
+      <AddMovie
+        onClick={ useCallback }
+      />
       <MovieLibrary />
     </div>
   );
