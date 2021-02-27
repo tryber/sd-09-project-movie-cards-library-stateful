baa200ba567d0e36d3dd4429e548265372441452
Maurice Alves <maurice.estevam@gmail.com>
2021-02-27 13:17:19 -0300

    Requisito - 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,37 @@
 // implement AddMovie component here
-/* import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      movies: [...movies],
+      /* searchText: '',
+      bookmarkedOnly: 'false',
+      selectedGenre: '', */
+    };
+  }
 
-    return();
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+        <MovieList movies={ movies } />
+      </div>
+    );
   }
 }
 
-export default MovieLibrary; */
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
