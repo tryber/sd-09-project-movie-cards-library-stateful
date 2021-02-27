29a79666534637221db8f0aca93fbd3f79a943a8
alailsonko <aalailson3@gmail.com>
2021-02-27 15:15:42 -0300

    searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -22,6 +22,32 @@ class SearchBar extends React.Component {
             value={ searchText }
           />
         </label>
+        <label
+          htmlFor="checkbox-input"
+          data-testid="checkbox-input-label"
+        >
+          Mostrar somente favoritos
+          <input
+            onChange={ onBookmarkedChange }
+            checked={ bookmarkedOnly }
+            type="checkbox"
+            data-testid="checkbox-input"
+          />
+        </label>
+
+        <label data-testid="select-input-label" htmlFor="select-input">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            onChange={ onSelectedGenreChange }
+            value={ selectedGenre }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
