e391db0de292eab04027c10096b80c4fa79f3638
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-02-28 12:17:01 -0300

    Requisito 15 cria componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>MovieLibrary</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
