2a15c11ae9c5dffac83fad2bcff11059d84be6fd
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-28 20:21:43 -0300

    ajustes lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -9,10 +9,10 @@ class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: props.movies,
+      // searchText: '',
+      // bookmarkedOnly: false,
+      // selectedGenre: '',
+      // movies: props.movies,
     };
   }
 
