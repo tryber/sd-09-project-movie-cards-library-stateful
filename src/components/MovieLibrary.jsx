edd3a0c6a9af20e3d9ab9c9816c74fa342758aae
danielfasanaro <danielfasanaro@gmail.com>
2021-02-28 17:09:07 -0300

    15th Requiriment: Implement MovieLibrary component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,10 @@
-// implement AddMovie component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { props: { movies } } = this;
+    return (<p>MovieLibrary</p>);
+  }
+}
+
+export default MovieLibrary;
