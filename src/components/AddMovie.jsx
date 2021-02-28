bb039bb516ee0c677555c1636f979c1bb399c648
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-02-28 20:43:22 -0300

    fix erros

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,12 @@
 // implement AddMovie component here
 import React from 'react';
 import PropTypes from 'prop-types';
-import LabelAndTitle from './LabelAndTitle';
-import LabelAndSubtitle from './LabelAndSubtitle';
-import LabelAndImage from './LabelAndImage';
-import LabelAndTextarea from './LabelAndTextarea';
-import LabelAndNumber from './LabelAndNumber';
-import LabelAndSelect from './LabelAndSelect';
+import LabelAndTitle from './LabelAndTitle.jsx';
+import LabelAndSubtitle from './LabelAndSubtitle.jsx';
+import LabelAndImage from './LabelAndImage.jsx';
+import LabelAndTextarea from './LabelAndTextarea.jsx';
+import LabelAndNumber from './LabelAndNumber.jsx';
+import LabelAndSelect from './LabelAndSelect.jsx';
 
 class AddMovie extends React.Component {
   constructor(props) {
@@ -24,7 +24,8 @@ class AddMovie extends React.Component {
 
   changeValue(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    if (name !== 'rating') this.setState({ [name]: value });
+    else this.setState({ [name]: Number(value) });
   }
 
   render() {
