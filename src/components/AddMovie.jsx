1abbc49a7734a144a1156e52e03bd8b6d92573ee
Felipe Rocha <felipe.sr27@gmail.com>
2021-03-01 09:51:35 -0300

    Requirement 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: 'action',
+  //   };
+  // }
+
   render() {
+    // const {
+    //   onClick
+    // } = this.props;
     return (
-      <div />
+      <form data-testid="add-movie-form" />
     );
   }
 }
+
+// AddMovie.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+// };
 
 export default AddMovie;
