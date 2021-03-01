364250f6d05ae110446e7956f260e9ecfc33180d
cleber-teixeira <cleberlt@gmail.com>
2021-02-28 21:34:54 -0300

    Requisito 05 - Renderizar um "select" dentro do formulário em "<SearchBar />". :white_check_mark:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,6 +10,8 @@ class SearchBar extends React.Component {
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
     } = this.props;
     return (
       <form htmlFor data-testid="search-bar-form">
@@ -33,6 +35,21 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label htmlFor="selected" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            type="secect"
+            id="selected"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
@@ -43,6 +60,8 @@ SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
