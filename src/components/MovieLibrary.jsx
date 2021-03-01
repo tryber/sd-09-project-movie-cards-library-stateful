e2dbf052e61d9980aa0b58efb874c97e38dd4862
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-01 18:06:14 -0300

    updating SearchBar State

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -8,24 +9,44 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.updateState = this.updateState.bind(this);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: this.props.movies,
+      movies: [ ...this.props.movies ],
     };
+  }
+
+  updateState({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      ...this.state,
+      [name]: value,
+    })
   }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.state.movies} />
+        <SearchBar
+          searchText={ this.state.searchText }
+          onSearchTextChange={ this.updateState }
+          bookmarkedOnly={ this.state.bookmarkedOnly }
+          onBookmarkedChange={ this.updateState }
+          selectedGenre={ this.state.selectedGenre }
+          onSelectedGenreChange={ this.updateState } />
+        <MovieList movies={ this.state.movies } />
         <AddMovie />
       </div>
     );
   }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.array,
 }
 
 export default MovieLibrary;
