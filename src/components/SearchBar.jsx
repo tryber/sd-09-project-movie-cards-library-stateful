79d9864d8837900ba8077f1b793e6484d2694676
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-01 12:56:12 -0300

    SearchBar form completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,42 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label">
+            Inclui o texto:
+            <input 
+              data-testid="text-input" 
+              onChange={this.props.onSearchTextChange}
+              value={this.props.searchText}
+              type="text"
+              name=""
+              id=""/>
+          </label>
+          <label data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input 
+              data-testid="checkbox-input"
+              onChange={this.props.onBookmarkedChange} 
+              checked={this.props.bookmarkedOnly} 
+              type="checkbox" 
+              name="" 
+              id=""/>
+          </label>
+          <label data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              data-testid="select-input"
+              onChange={this.props.onSelectedGenreChange}
+              value={this.props.selectedGenre}
+              name="" 
+              id="">
+                <option data-testid="select-option" value="">Todos</option>
+                <option data-testid="select-option" value="action">Ação</option>
+                <option data-testid="select-option" value="comedy">Comédia</option>
+                <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
       </div>
     )
   }
