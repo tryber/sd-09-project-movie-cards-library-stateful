17b9a5a7278966737b83b01fe42320d23d5fe9b3
Rafael Pufal <rafael.pufal23@gmail.com>
2021-03-01 17:00:28 -0300

    add filtro correto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -51,7 +51,7 @@ class MovieLibrary extends Component {
   //   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
