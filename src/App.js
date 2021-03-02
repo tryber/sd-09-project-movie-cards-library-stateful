93360f321fe04f3febdc02ea8dc4606e3057e1b2
Perycles floriano <perycles_reis@hotamil.com>
2021-03-02 15:27:45 -0300

    requisitos 10,11 e 12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -7,13 +7,17 @@ class AddMovie extends Component {
     this.inputTitle = this.inputTitle.bind(this);
     this.textContentInput = this.textContentInput.bind(this);
     this.inputSubtitle = this.inputSubtitle.bind(this);
+    this.inputImage = this.inputImage.bind(this);
+    this.inputSinopse = this.inputSinopse.bind(this);
+    this.inputRating = this.inputRating.bind(this);
+    this.inputSelected = this.inputSelected.bind(this);
     this.state = {
       subtitle: '',
       title: '',
-      // imagePath: '',
-      // storyline: '',
-      // rating: 0,
-      // genre: 'action',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
     };
   }
 
@@ -58,11 +62,87 @@ class AddMovie extends Component {
     return input;
   }
 
+  inputImage() {
+    const { imagePath } = this.state;
+    const input = (
+      <label data-testid="image-input-label" htmlFor="*">
+        Imagem:
+        <input
+          type="text"
+          name="imagePath"
+          value={ imagePath }
+          data-testid="image-input"
+          onChange={ this.textContentInput }
+        />
+      </label>
+    );
+    return input;
+  }
+
+  inputSinopse() {
+    const { storyline } = this.state;
+    const textarea = (
+      <label data-testid="storyline-input-label" htmlFor="*">
+        Sinopse:
+        <textarea
+          data-testid="storyline-input"
+          name="storyline"
+          value={ storyline }
+          cols="30"
+          rows="10"
+          onChange={ this.textContentInput }
+          placeholder="Digite aqui a sinopse do filme."
+        />
+      </label>
+    );
+    return textarea;
+  }
+
+  inputRating() {
+    const { rating } = this.state;
+    const input = (
+      <label data-testid="rating-input-label" htmlFor="*">
+        Avaliação:
+        <input
+          data-testid="rating-input"
+          type="number"
+          name="rating"
+          value={ rating }
+          onChange={ this.textContentInput }
+        />
+      </label>
+    );
+    return input;
+  }
+
+  inputSelected() {
+    const { genre } = this.state;
+    const input = (
+      <label data-testid="genre-input-label" htmlFor="*">
+        Gênero:
+        <select
+          name="genre"
+          value={ genre }
+          data-testid="genre-input"
+          onChange={ this.textContentInput }
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+    return input;
+  }
+
   render() {
     return (
       <form data-testid="add-movie-form">
         { this.inputTitle() }
         { this.inputSubtitle() }
+        { this.inputImage() }
+        { this.inputSinopse() }
+        { this.inputRating() }
       </form>
     );
   }
