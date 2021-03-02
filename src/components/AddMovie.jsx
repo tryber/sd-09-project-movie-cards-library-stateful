a7db80ce260f2fc0a349586174036358871d4541
diego-vini <analistamm@gmail.com>
2021-03-02 11:10:50 -0300

    Requisitos 1 ao 8 ajustando espaços

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,37 +5,36 @@ class AddMovie extends React.Component {
   constructor() {
     super();
     this.state = {
-        subtitle: '',
-        title: '',
-        imagePath: '',
-        storyline: '',
-        rating: 0,
-        genre: 'action'
-    }
-}
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
 
-handleTitle() {
-    console.log('title')
-}
+  handleTitle() {
+    console.log('title');
+  }
 
-    render() {
-        return(
-           <div>
-             <form data-testid="add-movie-form"> 
-               <label data-testid="title-input-label">
-                  Título
-                  <input type="text" 
-                  title={this.state.title}
-                  data-testid="title-input"
-                  onChange={this.handleTitle}
-                  />
-               </label>
-               <button onClick={() =>{}}></button>
-             </form>
-               
-           </div> 
-        )
-    }
+  render() {
+    return(
+      <div>
+        <form data-testid="add-movie-form">
+          <label data-testid="title-input-label">
+            Título
+            <input type="text" 
+              title={ this.state.title }
+                data-testid="title-input"
+                onChange={ this.handleTitle }
+            />
+          </label>
+          <button onClick={ () => {} }></button>
+        </form>               
+    </div> 
+    );
+  }
 }
 
 export default AddMovie;
