da0854b40a307c86edf7109fda4c1d778c38c3a8
Miguel Dantas <miiguelalb@gmail.com>
2021-03-02 11:44:00 -0300

    Update MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,22 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import MovieList from './MovieList';
+import SeachBar from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SeachBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary
