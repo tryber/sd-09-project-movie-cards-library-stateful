8cc76b8cbd7f6896238665103d445c788b8ec6a7
Matheus Monteiro <monteiro@pop-os.localdomain>
2021-03-02 15:47:42 -0300

    commit bomba de requisitos feitos ate o momento

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
+  render() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre,} = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.array,
+}.isRequired;
+
+export default MovieLibrary;
