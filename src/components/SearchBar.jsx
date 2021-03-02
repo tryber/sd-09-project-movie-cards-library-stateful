93a67162dd2a101343240b944bd3890092d2aaa8
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-03-01 23:34:53 -0300

    Criando e renderizando SearchBar e AddMovie

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    return <p>teste</p>;
+  }
+}
+
+export default SearchBar;
