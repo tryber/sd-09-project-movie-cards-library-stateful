0b4683208ffae3e2c8169bfec3e236e6f613ef46
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-03-03 01:53:25 -0300

    Requisitos 1 ao 3

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    return <p>teste AssMocie</p>;
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    return <p>teste</p>;
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="textInput">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
+    );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
