cb9e53101a6c6375e3c4a43ff6906c8693850dae
Marina Alane <marina.alane@outlook.com>
2021-03-02 21:59:36 -0300

    add componente addMOvie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* uma text area dentro de um formulário */}
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   render() {
     return (
       <section>
         <SearchBar />
+        <MovieList />
+        <AddMovie />
       </section>
     );
   }
