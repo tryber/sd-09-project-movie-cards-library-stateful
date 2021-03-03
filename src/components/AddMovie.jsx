ec891189f29e26e7f3152b9d679fdeb968645b2f
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-03 13:38:21 -0300

    fixing issues

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,6 +12,7 @@ const INITIAL_STATE = {
 };
 
 class AddMovie extends Component {
+
   constructor(props) {
     super(props);
     this.state = INITIAL_STATE;
@@ -32,8 +33,9 @@ class AddMovie extends Component {
   }
 
   sendInformations(e) {
+    const state = this.state
     e.preventDefault();
-    this.props.onClick(this.state);
+    this.props.onClick(state);
     this.resetInformations();
   }
 
@@ -47,79 +49,47 @@ class AddMovie extends Component {
       genre: 'action',
     });
   }
-
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <div>
         <form data-testid="add-movie-form">
-          <label data-testid="title-input-label">
+          <label data-testid="title-input-label" id="inputTitle">
             Título
-            <input
-              onChange={ this.updateStateValue }
-              data-testid="title-input"
-              value={ this.state.title }
-              type="text"
-              name="title"
-              id=""/>
+            <input onChange={ this.updateStateValue } data-testid="title-input"
+              value={ title } type="text" name="title" id="inputTitle"/>
           </label>
-          <label data-testid="subtitle-input-label">
+          <label data-testid="subtitle-input-label" id="inputSubtitle">
             Subtítulo
-            <input
-              onChange={ this.updateStateValue }
-              data-testid="subtitle-input"
-              value={this.state.subtitle}
-              type="text"
-              name="subtitle"
-              id=""/>
+            <input onChange={ this.updateStateValue } data-testid="subtitle-input"
+              value={ subtitle } type="text" name="subtitle" id="inputSubtitle"/>
           </label>
-          <label data-testid="image-input-label">
+          <label data-testid="image-input-label" id="inputImage">
             Imagem
-            <input
-              onChange={ this.updateStateValue }
-              data-testid="image-input"
-              value={ this.state.imagePath }
-              type="text"
-              name="imagePath"
-              id=""/>
+            <input onChange={ this.updateStateValue } data-testid="image-input"
+              value={ imagePath } type="text" name="imagePath" id="inputImage"/>
           </label>
-          <label data-testid="storyline-input-label">
+          <label data-testid="storyline-input-label" id="inputSinopse">
             Sinopse
-            <textarea
-              onChange={ this.updateStateValue }
-              data-testid="storyline-input"
-              value={ this.state.storyline }
-              name="storyline"
-              id=""
-              cols="30"
-              rows="10"
+            <textarea onChange={ this.updateStateValue } data-testid="storyline-input"
+              value={ storyline } name="storyline" id="inputSinopse" cols="30" rows="10"
             ></textarea>
           </label>
-          <label data-testid="rating-input-label">
+          <label data-testid="rating-input-label" id="inputAvaliacao">
             Avaliação
-            <input
-              onChange={ this.updateStateValue }
-              data-testid="rating-input"
-              value={ this.state.rating }
-              type="number"
-              name="rating"
-              id=""/>
+            <input onChange={ this.updateStateValue } data-testid="rating-input"
+              value={ rating } type="number" name="rating" id="inputAvaliacao"/>
           </label>
-          <label data-testid="genre-input-label">
+          <label data-testid="genre-input-label" id="inputGenero">
             Gênero
-            <select
-              onChange={ this.updateStateValue }
-              value={ this.state.genre }
-              data-testid="genre-input"
-              name="genre">
+            <select onChange={ this.updateStateValue } value={ genre }
+              data-testid="genre-input" name="genre" id="inputGenero">
                 <option data-testid="genre-option" value="action">Ação</option>
                 <option data-testid="genre-option" value="comedy">Comédia</option>
                 <option data-testid="genre-option" value="thriller">Suspense</option>
             </select>
           </label>
-          <button
-            type='submit'
-            data-testid="send-button"
-            onClick={this.sendInformations}>
+          <button type='submit' data-testid="send-button" onClick={this.sendInformations}>
             Adicionar filme
           </button>
         </form>
