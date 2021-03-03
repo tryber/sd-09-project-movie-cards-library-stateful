1d975318832733914b49473a968605e6f5fceaa1
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-03-03 20:25:42 -0300

    Lesson: Adicionando inputs e labels.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,52 @@
-// implement AddMovie component here
+import React from "react";
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: "action",
+    };
+    this.randomChange = this.randomChange.bind(this);
+  }
+
+  randomChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    const { title, subtitle } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            value={ title }
+            data-testid="title-input"
+            itemID="title-input"
+            onChange={ this.randomChange }
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            itemID="subtitle-input"
+            onChange={ this.randomChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
