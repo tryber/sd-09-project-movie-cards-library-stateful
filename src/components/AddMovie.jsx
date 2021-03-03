848196973b8d4a0239f6e9052c077b8280bfa508
Leticia Lima <letylima96@gmail.com>
2021-03-03 16:23:23 -0300

    requisito 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,7 +8,7 @@ class AddMovie extends React.Component {
     this.state = {
       subtitle: '',
       title: '',
-      // imagePath: '',
+      imagePath: '',
       // storyline: '',
       // rating: 0,
       // genre: 'action',
@@ -21,7 +21,7 @@ class AddMovie extends React.Component {
   }
 
   render() {
-    const { title, subtitle } = this.state;
+    const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title-input" data-testid="title-input-label">
@@ -44,6 +44,17 @@ class AddMovie extends React.Component {
             onChange={ this.updateState2 }
             name="subtitle"
             itemID="subtitle-input"
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            value={ imagePath }
+            data-testid="image-input"
+            onChange={ this.updateState2 }
+            name="imagePath"
+            itemID="image-input"
           />
         </label>
       </form>
