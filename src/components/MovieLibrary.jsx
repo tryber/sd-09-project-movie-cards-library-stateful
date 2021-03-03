4e54ca82606bec5f4c4f80d0cf31f8b234406975
Marina Alane <marina.alane@outlook.com>
2021-03-02 21:49:38 -0300

    adc component Movie Library

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <section>
+        <SearchBar />
+      </section>
+    );
+  }
+}
+
+export default MovieLibrary;
