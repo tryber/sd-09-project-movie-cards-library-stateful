a34c86fce2748b21ef83e6e9477d6d399969e5e3
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-03-03 02:43:12 -0300

    Requisitos 4 e 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,18 +2,70 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
-  render() {
+  constructor() {
+    super();
+
+    this.input = this.input.bind(this);
+    this.checkBox = this.checkBox.bind(this);
+    this.select = this.select.bind(this);
+  }
+
+  input() {
     const { searchText, onSearchTextChange } = this.props;
     return (
+      <label data-testid="text-input-label" htmlFor="input">
+        Inclui o texto:
+        <input
+          type="text"
+          data-testid="text-input"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+      </label>
+    );
+  }
+
+  checkBox() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="checkBox">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
+  select() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label data-testid="select-input-label" htmlFor="select">
+        Filtrar por gênero
+        <select
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+          data-testid="select-input"
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="textInput">
-          Inclui o texto:
-          <input
-            data-testid="text-input"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-          />
-        </label>
+        { this.input() }
+        { this.checkBox() }
+        { this.select() }
       </form>
     );
   }
@@ -22,6 +74,10 @@ class SearchBar extends Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
