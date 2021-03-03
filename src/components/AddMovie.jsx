52ae1c751ddd6284dcb40315cec044dd9e3f7b96
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-03-03 17:37:14 -0300

    Requisito 6 a 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.changeContent = this.changeContent.bind(this);
+
+    this.state = {
+      // subtitle: '',
+      title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  changeContent({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <p>teste AssMocie</p>;
+    const {
+      title,
+    } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="inputTitle">
+          Título:
+          <input
+            type="text"
+            name="title"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.changeContent }
+          />
+        </label>
+
+      </form>
+    );
   }
 }
 
