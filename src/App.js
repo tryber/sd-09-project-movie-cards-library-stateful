0a1c50b6f977dbf00edff518ece7bae53b115fff
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-03-03 18:17:13 -0300

    Requisito 17 - Correçoes eslint

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieLibrary movies={ [] } />
+      <MovieLibrary movies={ [{}] } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -63,5 +64,9 @@ class MovieLibrary extends Component {
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
