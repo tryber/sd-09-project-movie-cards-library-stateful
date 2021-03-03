6e14cd4809bdfa8c80a089a8861e9510e4e22a82
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-03-03 12:30:23 -0300

    movieLibrary component fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SeachBar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Search bar</h2>
+      </div>
+    );
+  }
+}
+
+export default SeachBar;
