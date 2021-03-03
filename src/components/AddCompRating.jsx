93bb005e198d2cf23364cb91ca6d9f403b4875c1
diego-vini <analistamm@gmail.com>
2021-03-03 09:34:02 -0300

    Requisitos 1 ao 8 ajustando codigo_7

--- src/components/AddCompRating.jsx
+++ src/components/AddCompRating.jsx
@@ -2,36 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddCompRating extends React.Component {
-  inputRating(rating, handleChange) {
+  render() {
+    const { rating, handleChange } = this.props;
     return (
       <label htmlFor="rating-input" data-testid="rating-input-label">
         Avaliação
         <input
           type="number"
-          name="rating"
-          data-testid="rating-input"
-          id="rating-input"
-          onChange={ handleChange }
           value={ rating }
-          min="0"
-          max="5"
-          step="any"
+          data-testid="rating-input"
+          onChange={ handleChange }
         />
       </label>
     );
   }
-
-  render() {
-    const { rating, handleChange } = this.props;
-    const { inputRating } = this;
-    return (
-      <fieldset>
-        {inputRating(rating, handleChange)}
-      </fieldset>
-    );
-  }
 }
-
 
 AddCompRating.propTypes = {
   rating: PropTypes.number.isRequired,
