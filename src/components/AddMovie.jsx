4e5b7081d4d0cb498f233bc0143fa56deeec4c7a
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-03 11:56:35 -0300

    changed AddMovie to arrowFunctions

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,6 @@
 // implement AddMovie component here
 import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Genre from './structure/Genre';
 import ImagePath from './structure/ImagePath';
 import Rating from './structure/Rating';
@@ -8,8 +8,8 @@ import StoryLine from './structure/StoryLine';
 import Subtitle from './structure/Subtitle';
 import Title from './structure/Title';
 
-function AddMovie({ addMovie }) {
-  const [teste, setTeste] = useState({
+function AddMovie(props) {
+  const [inputed, setInputed] = useState({
     subtitle: '',
     title: '',
     imagePath: '',
@@ -18,17 +18,19 @@ function AddMovie({ addMovie }) {
     genre: 'action',
   });
 
-  const handleClick = ({ target }) => {
-    const { name, value } = target.value;
-    setTeste({
+  const handleClick = (e) => {
+    const { name, value } = e.target;
+    setInputed({
+      ...inputed,
       [name]: value,
     });
   };
 
-  const buttonHandler = () => {
-
-    addMovie = addMovie(teste);
-    setTeste({
+  const buttonHandler = (e) => {
+    e.preventDefault();
+    const { onClick } = props;
+    onClick(inputed);
+    setInputed({
       subtitle: '',
       title: '',
       imagePath: '',
@@ -41,12 +43,12 @@ function AddMovie({ addMovie }) {
   return (
     <div>
       <form data-testid="add-movie-form">
-        <Title value={ teste.title } onChange={ handleClick } />
-        <Subtitle value={ teste.subtitle } onChange={ handleClick } />
-        <ImagePath value={ teste.imagePath } onChange={ handleClick } />
-        <StoryLine value={ teste.storyLine } onChange={ handleClick } />
-        <Rating value={ teste.rating } onChange={ handleClick } />
-        <Genre value={ teste.genre } onChange={ handleClick } />
+        <Title value={ inputed.title } onChange={ handleClick } />
+        <Subtitle value={ inputed.subtitle } onChange={ handleClick } />
+        <ImagePath value={ inputed.imagePath } onChange={ handleClick } />
+        <StoryLine value={ inputed.storyLine } onChange={ handleClick } />
+        <Rating value={ inputed.rating } onChange={ handleClick } />
+        <Genre value={ inputed.genre } onChange={ handleClick } />
         <button data-testid="send-button" type="button" onClick={ buttonHandler }>
           Adicionar filme
         </button>
@@ -54,5 +56,9 @@ function AddMovie({ addMovie }) {
     </div>
   );
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
