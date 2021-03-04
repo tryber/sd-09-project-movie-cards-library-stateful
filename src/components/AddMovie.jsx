36b9c542cc577574ce4506d55c43eb89c7e38906
Micaela Alves <micaela.alves93@gmail.com>
2021-03-04 02:15:12 -0300

    Requisito 11 - renderize uma textarea

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,6 +25,26 @@ class AddMovie extends React.Component {
             onChange={ this.setState.title }
           />
         </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+          Subtítulo
+          <input
+            data-testid="subtitle-input"
+            type="text"
+            onChange={ this.setState.subtitle }
+          />
+        </label>
+        <label data-testid="image-input-label" htmlFor="image-input">
+          Imagem
+          <input
+            data-testid="image-input"
+            type="text"
+            onChange={ this.setState.imagePath }
+          />
+        </label>
+        <label data-testid="storyline-input-label" htmlFor="storyline-input">
+          Sinopse
+          <textarea data-testid="storyline-input" />
+        </label>
       </form>
     );
   }
