390bb4fadc86db9f06c0a1a7f0a83b2c0a765978
Kevin Fraga <kevinciolac@gmail.com>
2021-03-04 18:12:07 -0300

    :one: SearchBar Component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,10 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (<p>Teste</p>);
+  }
+}
+
+export default SearchBar;
