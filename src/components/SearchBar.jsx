898028775d0d939b07423a1efc0189ddba430392
Kevin Fraga <kevinciolac@gmail.com>
2021-03-04 19:17:31 -0300

    :three: SearchBar Component text input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <p>Teste</p>
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+        </label>
+        <input
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+          id="text-input"
+        />
       </form>
     );
   }
