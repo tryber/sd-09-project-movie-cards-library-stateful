cbebf973f44cc60671ff1a08ae585a2f28538406
Carine Becker <caarine.m@gmail.com>
2021-03-04 13:43:27 -0300

    requisito 16 de novo novamente

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -18,7 +18,47 @@ class MovieLibrary extends Component {
       movies,
     };
 
+    this.filterMovies = this.filterMovies.bind(this);
     this.insertMovie = this.insertMovie.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  }
+
+  handleChange(event, name) {
+    const { value, type, checked } = event.target;
+    const changedValue = type === 'checkbox' ? checked : value;
+    // se o tipo for uma checkbox e estiver checked ele altera o valor para false para tirar o checked e vice-versa
+    this.setState({ [name]: changedValue });
+  }
+
+  onBookmarkedChange(event) {
+    this.handleChange(event, 'bookmarkedOnly');
+  }
+
+  onSelectedGenreChange(event) {
+    this.handleChange(event, 'selectedGenre');
+  }
+
+  onSearchTextChange(event) {
+    this.handleChange(event, 'searchText');
+  }
+
+  filterMovies() {
+    const { bookmarkedOnly, movies } = this.state;
+
+    let newMovies = movies;
+
+    // filtra por favoritos
+    if (bookmarkedOnly) {
+      newMovies = newMovies.filter(({ bookmarked }) => bookmarked);
+    }
+
+    return newMovies;
+    // filtra por texto
+
+    // filtra por genero
   }
 
   insertMovie(newMovie) {
@@ -33,17 +73,20 @@ class MovieLibrary extends Component {
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar
           searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
           bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
           selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
         />
         <AddMovie onClick={ this.insertMovie } />
-        <MovieList movies={ movies } />
+        <MovieList movies={ this.filterMovies() } />
       </div>
     );
   }
