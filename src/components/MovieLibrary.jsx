b7f2fc4c1892d523aebcf0815ed3ce4eafdf6b79
Pedro Amaral <amaralph1995@gmail.com>
2021-03-04 17:41:24 -0300

    Cria o componente SearchBar com um formulario contendo um input

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
 // implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // const { searchText, onSearchTextChange, bookmarkedOnly,
+    //   onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+// MovieLibrary.propTypes = {
+//   searchText: PropTypes.string,
+//   onSearchTextChange: PropTypes.func,
+//   bookmarkedOnly: PropTypes.bool,
+//   onBookmarkedChange: PropTypes.func,
+//   selectedGenre: PropTypes.string,
+//   onSelectedGenreChange: PropTypes.func,
+// }
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,28 @@
 // implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return(
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">Inclui o texto:
+          <input
+            type="text"
+            data-testid="text-input"
+            value={ this.props.searchText }
+            onChange={ this.props.onSearchTextChange }
+          />
+        </label>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
+
+        // searchText={ searchText }
+        // onSearchTextChange={ onSearchTextChange }
+        // bookmarkedOnly={ bookmarkedOnly }
+        // onBookmarkedChange={ onBookmarkedChange }
+        // selectedGenre={ selectedGenre }
+        // onSelectedGenreChange={ onSelectedGenreChange }
