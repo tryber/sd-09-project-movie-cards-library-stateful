7d3f3af85bad1ed5183a851427ba86400ad7a4d5
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-03-04 01:59:04 -0300

    Lesson: PASSA

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,30 @@
-// import React, { Component } from 'react';
+import React from 'react';
+import propTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      movies,
+    };
+  }
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
-
-// export default MovieLibrary;
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+MovieLibrary.propTypes = {
+  movies: propTypes.arrayOf(propTypes.object).isRequired,
+};
+export default MovieLibrary;
