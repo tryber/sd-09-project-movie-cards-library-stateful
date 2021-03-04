b3a8011f71af11f3431d319131f8404cde192bac
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-03-04 15:44:37 -0300

    Requisitos 15 e 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,44 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ () => {} }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ () => {} }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ () => {} }
+        />
+        <AddMovie />
+        <MovieList movies={ movies } />
+      </>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+
+export default MovieLibrary;
