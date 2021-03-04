b08cee351d772f7e2ad0a8f605ba1539702d9e3e
Micaela Alves <micaela.alves93@gmail.com>
2021-03-04 02:00:49 -0300

    Requisito 8 - Rebderize um input para o titulo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 /* import PropTypes from 'prop-types'; */
 
 class AddMovie extends React.Component {
-/* constructor(props) {
+  constructor(props) {
     super(props);
-      this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
+    this.state = {
+      /* subtitle: '',
+      title: ''
+      /* imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action',
+      genre: 'action', */
     };
-  } */
+  }
 
   render() {
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            onChange={ this.setState.title }
+          />
+        </label>
+      </form>
     );
   }
 }
