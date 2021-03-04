e44d8f0d774623880a858051477b02a1f0e453c9
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-04 10:58:35 -0300

    fixing issues

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -50,6 +50,38 @@ class AddMovie extends Component {
     });
   }
 
+  inputTextArea() {
+    const { storyline } = this.state;
+    return (
+      <textarea
+        onChange={ this.updateStateValue }
+        data-testid="storyline-input"
+        value={ storyline }
+        name="storyline"
+        id="inputSinopse"
+        cols="30"
+        rows="10"
+      />
+    )
+  }
+
+  inputSelect() {
+    const { genre } = this.state;
+    return (
+      <select
+        onChange={ this.updateStateValue }
+        value={ genre }
+        data-testid="genre-input"
+        name="genre"
+        id="inputGenero"
+      >
+        <option data-testid="genre-option" value="action">Ação</option>
+        <option data-testid="genre-option" value="comedy">Comédia</option>
+        <option data-testid="genre-option" value="thriller">Suspense</option>
+      </select>
+    )
+  }
+
   inputGen(id, name, type, value) {
     return (
       <input
@@ -57,14 +89,14 @@ class AddMovie extends Component {
         name={ name }
         type={ type }
         value={ value }
-        data-testid={ name !== "imagePath" ? `${name}-input` : "image-input" }
+        data-testid={ name !== 'imagePath' ? `${name}-input` : 'image-input' }
         onChange={ this.updateStateValue }
       />
     );
   }
 
   render() {
-    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    const { title, subtitle, imagePath, rating } = this.state;
     return (
       <div>
         <form data-testid="add-movie-form">
@@ -74,44 +106,26 @@ class AddMovie extends Component {
           </label>
           <label data-testid="subtitle-input-label" htmlFor="inputSubtitle">
             Subtítulo
-            { this.inputGen('inputSubtitle', 'subtitle', 
+            { this.inputGen('inputSubtitle', 'subtitle',
               'text', subtitle, 'subtitle-input') }
           </label>
           <label data-testid="image-input-label" htmlFor="inputImage">
             Imagem
-            { this.inputGen('inputImage', 'imagePath', 
+            { this.inputGen('inputImage', 'imagePath',
               'text', imagePath, 'image-input') }
           </label>
           <label data-testid="storyline-input-label" htmlFor="inputSinopse">
             Sinopse
-            <textarea
-              onChange={ this.updateStateValue }
-              data-testid="storyline-input"
-              value={ storyline }
-              name="storyline"
-              id="inputSinopse"
-              cols="30"
-              rows="10"
-            />
+            { this.inputTextArea() }
           </label>
           <label data-testid="rating-input-label" htmlFor="inputAvaliacao">
             Avaliação
-            { this.inputGen('inputAvaliacao', 'rating', 
+            { this.inputGen('inputAvaliacao', 'rating',
               'number', rating, 'rating-input') }
           </label>
           <label data-testid="genre-input-label" htmlFor="inputGenero">
             Gênero
-            <select
-              onChange={ this.updateStateValue }
-              value={ genre }
-              data-testid="genre-input"
-              name="genre"
-              id="inputGenero"
-            >
-              <option data-testid="genre-option" value="action">Ação</option>
-              <option data-testid="genre-option" value="comedy">Comédia</option>
-              <option data-testid="genre-option" value="thriller">Suspense</option>
-            </select>
+            { this.inputSelect() }
           </label>
           <button
             type="submit"
