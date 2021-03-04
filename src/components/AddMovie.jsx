d6a401f8c0f2cdba5aea4bff82e112e2c63dd991
Pedro Amaral <amaralph1995@gmail.com>
2021-03-04 20:05:26 -0300

    Tenta corrigir ESlint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class AddMovie extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       subtitle: '',
@@ -12,20 +12,16 @@ class AddMovie extends React.Component {
       storyline: '',
       rating: 0,
       genre: 'action',
-    }
+    };
   }
 
   render() {
     const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
-
     const handleChange = (event) => {
-      this.setState({
-        [event.target.id]: event.target.value,
-      });
+      this.setState({ [event.target.id]: event.target.value });
     }
-
-    const sendForm = (event) => {
+    const sendForm = () => {
       this.setState({
         subtitle: '',
         title: '',
@@ -36,44 +32,36 @@ class AddMovie extends React.Component {
       });
       onClick();
     }
-
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title">
           Título
           <input type="text" value={ title }
-            data-testid="title-input" id="title" onChange={ handleChange }
-          />
+            data-testid="title-input" id="title" onChange={ handleChange } />
         </label>
         <label data-testid="subtitle-input-label" htmlFor="subtitle">
           Subtítulo
           <input type="text" value={ subtitle }
-            data-testid="subtitle-input" id="subtitle" onChange={ handleChange }
-          />
+            data-testid="subtitle-input" id="subtitle" onChange={ handleChange } />
         </label>
         <label data-testid="image-input-label" htmlFor="imagePath">
           Imagem
           <input type="text" value={ imagePath }
-            data-testid="image-input" id="imagePath" onChange={ handleChange }
-          />
+            data-testid="image-input" id="imagePath" onChange={ handleChange } />
         </label>
         <label data-testid="storyline-input-label" htmlFor="storyline">
           Sinopse
           <textarea value={ storyline } data-testid="storyline-input"
-            id="storyline" onChange={ handleChange }
-          />
+            id="storyline" onChange={ handleChange } />
         </label>
         <label data-testid="rating-input-label" htmlFor="rating">
           Avaliação
           <input type="number" value={ rating }
-            data-testid="rating-input" id="rating" onChange={ handleChange }
-          />
+            data-testid="rating-input" id="rating" onChange={ handleChange } />
         </label>
         <label data-testid="genre-input-label" htmlFor="genre">
           Gênero
-          <select value={ genre } data-testid="genre-input"
-            id="genre" onChange={ handleChange }
-          >
+          <select value={ genre } data-testid="genre-input" id="genre" onChange={ handleChange }>
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
