2f97bd72879e8cdd19b0897622ae22bf3f86d7ee
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-03-04 01:08:23 -0300

    Lesson: criando components.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,79 +1,75 @@
 import React from 'react';
+import Button from './Button';
+import Title from './labels/Title';
+import Subtitle from './labels/Subtitle';
+import Storyline from './labels/Storyline';
 
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
       rating: 0,
-      genre: 'action',
+      genre: "action",
     };
     this.randomChange = this.randomChange.bind(this);
+    this.addMovie = this.addMovie.bind(this);
   }
 
   randomChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
 
+  addMovie() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
-    const { title, subtitle, imagePath, storyline, rating } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
-          Título
-          <input
-            type="text"
-            name="title"
-            value={ title }
-            data-testid="title-input"
-            itemID="title-input"
-            onChange={ this.randomChange }
-          />
-        </label>
-        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            type="text"
-            name="subtitle"
-            value={ subtitle }
-            data-testid="subtitle-input"
-            itemID="subtitle-input"
-            onChange={ this.randomChange }
-          />
-        </label>
-        <label htmlFor="image-input" data-testid="image-input-label">
-          Imagem
-          <input
-            type="text"
-            name="imagePath"
-            value={ imagePath }
-            data-testid="image-input"
-            itemID="image-input"
-            onChange={ this.randomChange }
-          />
-        </label>
-        <label htmlFor="storyLine-input" data-testid="storyline-input-label">
-          Sinopse
-          <input
-            type="text"
-            name="storyline"
-            value={ storyline }
-            data-testid="storyline-input"
-            itemID="storyLine-input"
-            onChange={ this.randomChange }
-          />
-        </label>
-        <label htmlFor="" data-testid="rating-input-label">
+        <Title title={ title } randomChange={ this.randomChange } />
+        <Subtitle subtitle={ subtitle } randomChange={ this.randomChange } />
+        <Imagepath image={ imagePath } randomChange={ this.randomChange } />
+        <Storyline storyline={ storyline } randomChange={ this.randomChange } />
+        <label htmlFor="rating-input" data-testid="rating-input-label">
           Avaliação
           <input
-          type="text"
-          value={ rating }
+            type="number"
+            name="rating"
+            value={ rating }
+            data-testid="rating-input"
+            itemID="rating-input"
+            onChange={ this.randomChange }
           />
-
         </label>
+        <label htmlFor="genre-input" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="genre"
+            value={ genre }
+            data-testid="genre-input"
+            itemID="genre-input"
+            onChange={ this.randomChange }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+        <Button onClick={ this.addMovie } />
       </form>
     );
   }
