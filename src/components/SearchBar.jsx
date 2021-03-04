27aa8c91f3c0670cca1637c21017637b914c12d3
Micaela Alves <micaela.alves93@gmail.com>
2021-03-04 00:31:19 -0300

    Requisito 3 - renderize input Searchtext

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,32 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    /*     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props; */
-    return (<form data-testid="search-bar-form" />);
+    const { onSearchTextChange, searchText } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>);
   }
 }
+SearchBar.defaultProps = {
+  onSearchTextChange: '',
+  searchText: '',
+};
+
+SearchBar.propTypes = {
+  onSearchTextChange: PropTypes.func,
+  searchText: PropTypes.string,
+};
 
 export default SearchBar;
