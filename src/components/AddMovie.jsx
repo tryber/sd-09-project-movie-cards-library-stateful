0604baa52bde02c6e4d97b4abf2114ab4dfab481
Micaela Alves <micaela.alves93@gmail.com>
2021-03-04 10:04:05 -0300

    Requisito 13 - renderize um select para o genero

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -53,6 +53,17 @@ class AddMovie extends React.Component {
             onChange={ this.setState.rating }
           />
         </label>
+        <label data-testid="genre-input-label" htmlFor="genre-input">
+          Gênero
+          <select
+            data-testid="genre-input"
+            onChange={ this.setState.genre }
+          >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
