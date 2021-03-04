b90779cb1f1c0b00c0f7b78fe5a73775bb45b584
Kevin Fraga <kevinciolac@gmail.com>
2021-03-04 18:21:23 -0300

    :two: SearchBar Component form tag - fix :one: minor fixes for linter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <form data-testid="search-bar-form"></form>  
+      <form data-testid="search-bar-form">
+        <p>Teste</p>
+      </form>
     );
   }
 }
