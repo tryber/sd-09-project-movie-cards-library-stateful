9dd3ef582aea9d6c1601355a9db63f0cf3cec29e
Pedro Amaral <amaralph1995@gmail.com>
2021-03-04 19:31:49 -0300

    Cria input pra receber imagePath no component AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -20,28 +20,38 @@ class AddMovie extends React.Component {
 
     const handleChange = (event) => {
       this.setState({
-        [event.target.name]: event.target.value,
+        [event.target.id]: event.target.value,
       })
     }
     return(
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label">
+        <label data-testid="title-input-label" for="title">
           Título
           <input
             type="text"
             value={ this.state.title }
             data-testid="title-input"
-            name="title"
+            id="title"
             onChange={ handleChange }
           />
         </label>
-        <label data-testid="subtitle-input-label">
+        <label data-testid="subtitle-input-label" for="subtitle">
           Subtítulo
           <input
             type="text"
             value={ this.state.subtitle }
             data-testid="subtitle-input"
-            name="subtitle"
+            id="subtitle"
+            onChange={ handleChange }
+          />
+        </label>
+        <label data-testid="image-input-label" for="">
+          Imagem
+          <input
+            type="text"
+            value={ this.state.imagePath }
+            data-testid="image-input"
+            id="imagePath"
             onChange={ handleChange }
           />
         </label>
