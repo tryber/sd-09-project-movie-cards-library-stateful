325cac8a8ead4b4fbd3280acaa1396f0a56de113
Pedro Amaral <amaralph1995@gmail.com>
2021-03-04 19:25:53 -0300

    Cria um input de texto pra receber o subtitulo em AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -29,12 +29,22 @@ class AddMovie extends React.Component {
           Título
           <input
             type="text"
+            value={ this.state.title }
             data-testid="title-input"
             name="title"
             onChange={ handleChange }
           />
         </label>
-
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            value={ this.state.subtitle }
+            data-testid="subtitle-input"
+            name="subtitle"
+            onChange={ handleChange }
+          />
+        </label>
       </form>
     )
   }
