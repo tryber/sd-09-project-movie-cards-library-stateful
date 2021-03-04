840d846fef69321ce5da00b3a8bebaa637d4a28f
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-03-04 01:25:25 -0300

    commit to backp idea

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,40 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChangeMovie = this.handleChangeMovie.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChangeMovie({ target }) {
+    const { name } = target;
+    const { value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form data-testid="add-movie-form">
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,51 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import movies from '../data';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChangeLibrary = this.handleChangeLibrary.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [...movies],
+    };
+  }
+
+  handleChangeLibrary({ target }) {
+    const { name } = target;
+    const value = target.value;
+    // declação coringa vista no conteudo
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          movies={ this.state.movies }
+          searchText={ this.state.searchText }
+          handleChangeLibrary={ this.handleChangeLibrary }
+          bookmarkedOnly={ this.state.bookmarkedOnly }
+          selectedGenre={ this.state.selectedGenre }
+        />
+        <AddMovie
+        movies={ this.movies }
+        handleChangeLibrary={ this.handleChangeLibrary }
+        />
+        <MovieList />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
