846829e545d4edf4c52033a23b71c4596f9003d8
Micaela Alves <micaela.alves93@gmail.com>
2021-03-04 00:49:25 -0300

    Requisito 04 - renderize unput tipo checkbox

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { onSearchTextChange, searchText } = this.props;
+    const { onSearchTextChange, searchText, onBookmarkedChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="text-input">
@@ -16,17 +16,29 @@ class SearchBar extends React.Component {
             onChange={ onSearchTextChange }
           />
         </label>
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+          Mostrar somente favoritos
+          <input
+            checked="bookmarkedOnly"
+            data-testid="checkbox-input"
+            type="checkbox"
+            onChange={ onBookmarkedChange }
+          />
+        </label>
+
       </form>);
   }
 }
 SearchBar.defaultProps = {
   onSearchTextChange: '',
   searchText: '',
+  onBookmarkedChange: '',
 };
 
 SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func,
   searchText: PropTypes.string,
+  onBookmarkedChange: '',
 };
 
 export default SearchBar;
