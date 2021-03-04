ac270aed613e141beb353d1db7483031f8633a93
Pedro Amaral <amaralph1995@gmail.com>
2021-03-04 19:40:12 -0300

    Adiciona inputs pra caturar rating e storyline em addMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,46 +17,67 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating } = this.state;
 
     const handleChange = (event) => {
       this.setState({
         [event.target.id]: event.target.value,
-      })
+      });
     }
+
     return(
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" for="title">
+        <label data-testid="title-input-label" htmlFor="title">
           Título
           <input
             type="text"
-            value={ this.state.title }
+            value={ title }
             data-testid="title-input"
             id="title"
             onChange={ handleChange }
           />
         </label>
-        <label data-testid="subtitle-input-label" for="subtitle">
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">
           Subtítulo
           <input
             type="text"
-            value={ this.state.subtitle }
+            value={ subtitle }
             data-testid="subtitle-input"
             id="subtitle"
             onChange={ handleChange }
           />
         </label>
-        <label data-testid="image-input-label" for="">
+        <label data-testid="image-input-label" htmlFor="imagePath">
           Imagem
           <input
             type="text"
-            value={ this.state.imagePath }
+            value={ imagePath }
             data-testid="image-input"
             id="imagePath"
             onChange={ handleChange }
           />
         </label>
+        <label data-testid="storyline-input-label" htmlFor="storyline">
+          Sinopse
+          <textarea
+            value={ storyline }
+            data-testid="storyline-input"
+            id="storyline"
+            onChange={ handleChange }
+          />
+        </label>
+        <label data-testid="rating-input-label" htmlFor="rating">
+          Avaliação
+          <input
+            type="number"
+            value={ rating }
+            data-testid="rating-input"
+            id="rating"
+            onChange={ handleChange }
+          />
+        </label>
       </form>
-    )
+    );
   }
 }
 
