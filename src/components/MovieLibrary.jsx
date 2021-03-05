c87870e6d4be221b54d613e354e5c526cc3b3ae5
Pedro Amaral <amaralph1995@gmail.com>
2021-03-04 23:22:45 -0300

    Cria e renderiza MovieLibrary com seus componentes internos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,36 +1,37 @@
 // implement AddMovie component here
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
-class MovieLibrary extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    const { movie } = this.props;
+class MovieLibrary extends React.Component {
+  constructor (props) {
+    super(props);
+    
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+  }
+  render() {    
+    
     return (
       <div>
-        <h2> My awesome movie library </h2>
         <SearchBar />
         <MovieList movies={ this.props.movies } />
         <AddMovie />
       </div>
-    );
+    )
   }
 }
 
 // MovieLibrary.propTypes = {
-//   searchText: PropTypes.string,
-//   onSearchTextChange: PropTypes.func,
-//   bookmarkedOnly: PropTypes.bool,
-//   onBookmarkedChange: PropTypes.func,
-//   selectedGenre: PropTypes.string,
-//   onSelectedGenreChange: PropTypes.func,
+//   movies: PropTypes.array,
 // }
 
 export default MovieLibrary;
