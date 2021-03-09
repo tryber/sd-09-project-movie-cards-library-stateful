9cd0ff6cbf916a82a7ceca249ea71c29a1bd515f
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-03-09 15:15:15 -0300

    Requesitos: :one: , :two: , :three: , :four: e :five:   :construction:  :rocket:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,62 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <section>
+        <form data-testid="search-bar-form">
+          <label htmlFor="Inclui o texto:" data-testid="text-input-label">
+            Inclui o texto
+            <input
+              data-testid="text-input"
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              name="searchBar"
+            />
+          </label>
+          <label htmlFor="Mostrar somente favoritos" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              name="bookMarkedCheckbox"
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label htmlFor="Filtrar por gênero" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              name="selectGenre"
+              onChange={ onSelectedGenreChange }
+              value={ selectedGenre }
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option"> Todos </option>
+              <option value="action" data-testid="select-option"> Ação </option>
+              <option value="comedy" data-testid="select-option"> Comédia</option>
+              <option value="thriller" data-testid="select-option"> Suspense </option>
+            </select>
+          </label>
+        </form>
+      </section>
+    );
+  }
+}
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
