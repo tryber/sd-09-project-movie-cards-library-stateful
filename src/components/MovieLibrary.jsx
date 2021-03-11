e529fcd5e5f04a5a61bee80ef8c87c4af9c8e6be
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-03-10 23:43:39 -0300

    teste

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class MovieLibrary extends React Component {
+export default class MovieLibrary extends React.Component {
   render() {
     return(
       <h1>MovieLibrary</h1>
