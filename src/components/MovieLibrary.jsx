a833ca9c590c0ded680323d4f6b0b2fc359d6e85
Micaela Alves <micaela.alves93@gmail.com>
2021-03-15 23:36:02 -0300

    requisito 16 - correções lint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,9 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import PropTypes from 'prop-types';
 
 class MovieLibrary extends Component {
   constructor(props) {
