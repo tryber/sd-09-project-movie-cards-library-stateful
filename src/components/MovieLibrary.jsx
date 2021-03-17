954e3b8f54c6504a3afabd6c64b780e3b3c0c646
Marina Alane <marina.alane@outlook.com>
2021-03-17 16:22:25 -0300

    renderiza movielist

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import PropTypes from '.prop-types';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -12,42 +13,42 @@ class MovieLibrary extends React.Component {
       bookmarkedOnly: false,
       selectedGenre: '',
       movies: props.movies,
-    }
+    };
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
     this.onClick = this.onClick.bind(this);
-      };
-    // Funções que vão atualizar os estados:
-    onSearchTextChange({ target }) {
-      const { value } = target;
-      this.setState({
-        searchText: value,
-      });
-    }
-  
-    onBookmarkedChange({ target }) {
-      const { name } = target;
-      const value = target.checked;
-      this.setState({
-        [name]: value,
-      });
-    }
-  
-    onSelectedGenreChange({ target }) {
-      const { value } = target;
-      this.setState({
-        selectedGenre: value,
-      });
-    }
-  
-    onClick(state) {
-      const { movies } = this.state;
-      this.setState({
-        movies: [...movies, state],
-      });
-    }
-  
+  }
+
+  // Funções que vão atualizar os estados:
+  onSearchTextChange({ target }) {
+    const { value } = target;
+    this.setState({
+      searchText: value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    const { name } = target;
+    const value = target.checked;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+    this.setState({
+      selectedGenre: value,
+    });
+  }
+
+  onClick(state) {
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, state],
+    });
+  }
 
   render() {
     const { movies } = this.props;
@@ -55,14 +56,15 @@ class MovieLibrary extends React.Component {
       <section>
         <SearchBar />
         <AddMovie />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.onClick } />
       </section>
     );
   }
+}
 
-  MovieLibrary.propTypes = {
-    searchText: PropTypes.string.isRequired,
-  };
-
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default MovieLibrary;
