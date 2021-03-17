21a02d9490ced694887808fb274ad45d4c84453a
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-03-17 20:50:25 -0300

    teste

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
 export default class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      storyline: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ storyline: event.target.value });
+  }
+
   render() {
+    const { storyline } = this.state;
     return (
       <div>
-        <h1>add movie</h1>
+        <label data-testid="storyline-input-label" htmlFor="textarea">
+          <textarea
+            htmlFor="textarea"
+            data-testid="storyline-input"
+            value={ storyline }
+            onChange={ this.handleChange }
+          />
+        </label>
       </div>
     );
   }
