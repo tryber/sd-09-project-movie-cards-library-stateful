7d9c3d82da196cdbe56a26b4ee60579499b3fbc9
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-03-17 23:52:09 -0300

    teste

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -38,7 +38,7 @@ export default class AddMovie extends React.Component {
               onChange={ this.handleChange }
             />
           </label>
-          {/* <label data-testid="subtitle-input-label" htmlFor="sb-input-label">
+          <label data-testid="subtitle-input-label" htmlFor="sb-input-label">
             Subtítulo
             <input
               type="text"
@@ -47,7 +47,7 @@ export default class AddMovie extends React.Component {
               value={ subtitle }
               onChange={ this.handleChange }
             />
-          </label> */}
+          </label>
         </form>
       </div>
     );
