039186e0e0cddc6a356000a5cc86882d5c7355d3
Kevin Fraga <kevinciolac@gmail.com>
2021-03-19 18:50:41 -0300

    :one::five: MovieLibrary Component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default MovieLibrary;
