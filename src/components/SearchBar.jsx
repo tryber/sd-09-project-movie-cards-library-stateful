53f9465d35ef434b0a59919598c0cb6e5731fd13
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-03-19 00:37:42 -0300

    teste1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TextInputLabelSearch from './TextInputLabelSearch';
-import CheckboxInputSearch from './CheckboxInputSearch';
+// import CheckboxInputSearch from './CheckboxInputSearch';
 
 class SearchBar extends React.Component {
   render() {
