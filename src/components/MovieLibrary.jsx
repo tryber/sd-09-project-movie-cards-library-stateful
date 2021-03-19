411a550292e12d366acd57a354d1abed413292b1
Kevin Fraga <kevinciolac@gmail.com>
2021-03-19 19:36:09 -0300

    :one::six: MovieLibrary Component initial state

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,53 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieCard from './MovieCard';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      null
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ () => '' }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ () => '' }
+          selectedGenre={ selectedGenre }
+          onSelecetedGenreChange={ () => '' }
+        />
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+        <AddMovie onClick={ () => '' } />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+      bookmarked: PropTypes.bool,
+      genre: PropTypes.string,
+    }),
+  ),
+}.isRequired;
 
 export default MovieLibrary;
