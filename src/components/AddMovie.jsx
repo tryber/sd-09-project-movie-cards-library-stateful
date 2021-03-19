261b383ad72cede13a290fe2a48abab682f28082
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-03-18 22:32:54 -0300

    teste

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import TitleForm from './TitleForm';
 import StoryLineForm from './StoryLineForm';
 import SubTitleInput from './SubtitleInput';
-// import ImageInput from './ImageInput';
+import ImageInput from './ImageInput';
 // import RatingInput from './RatingInput';
 // import SelectGender from './SelectGender';
 
@@ -42,15 +42,15 @@ class AddMovie extends React.Component {
 
   render() {
     // const { title, storyline, subtitle, imagePath, rating, genre } = this.state;
-    const { title, storyline, subtitle } = this.state;
+    const { title, storyline, subtitle, imagePath } = this.state;
     return (
       <div>
         <form data-testid="add-movie-form">
           <StoryLineForm value={ storyline } handleChange={ this.handleChange } />
           <TitleForm value={ title } handleChange={ this.handleChange } />
           <SubTitleInput value={ subtitle } handleChange={ this.handleChange } />
-          {/* <ImageInput value={ imagePath } handleChange={ this.handleChange } />
-          <RatingInput value={ rating } handleChange={ this.handleChange } />
+          <ImageInput value={ imagePath } handleChange={ this.handleChange } />
+          {/* <RatingInput value={ rating } handleChange={ this.handleChange } />
           <SelectGender value={ genre } handleChange={ this.handleChange } />
           <button
             data-testid="send-button"
