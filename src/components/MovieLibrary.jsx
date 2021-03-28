7edd0079ba6942f5aadfe2637345b6ed9f5fff12
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-03-28 17:48:48 -0300

    Requisitos de 1 a 6 falhando em 3 testes a serem resolvidos nos proximos requisitos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h2> Movie Library </h2>
+        <SearchBar />
+        <MovieList movies={ this.props.movies } />
+        <AddMovie />
+      </>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,64 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookMarkedOnly,
+      onBookMarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="addText" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="addText"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="favoritesCheckBox">
+          Mostrar somente favoritos
+          <input
+            id="favoritesCheckBox"
+            type="checkbox"
+            checked={ bookMarkedOnly }
+            onChange={ onBookMarkedChange }
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="byGenre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            id="byGenre"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookMarkedOnly: PropTypes.bool.isRequired,
+  onBookMarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
