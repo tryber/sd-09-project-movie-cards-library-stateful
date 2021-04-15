c466088351d7ec33dd5965064e825b5252fd56a1
Marina Alane <marina.alane@outlook.com>
2021-04-15 10:55:13 -0300

    FALTAM DOIS TESTES SCR

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,8 +13,12 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
+          <p data-testid="rating">
+            <Rating />
+            {rating}
+          </p>
         </div>
-        <Rating rating={ rating } />
+
       </div>
     );
   }
