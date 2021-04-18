2c267ab6ab41c72d92decd04d2f5fe9358db2274
jepaur5 <jepaur5@gmail.com>
2021-04-18 02:50:47 -0300

    implementando titulo.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          data-testid="title-input"
+          type="text"
+          name="title"
+          value={ value }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+Title.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
 export default Title;
