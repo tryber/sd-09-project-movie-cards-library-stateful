c37c3d9d13bf1ee683cfe92d39b7787e4d1f354c
jepaur5 <jepaur5@gmail.com>
2021-04-18 03:10:14 -0300

    implementando textoArea.jsx e imagenCamino.jsx

--- src/components/ImagePath.jsx
+++ src/components/ImagePath.jsx
@@ -1,1 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ImagePath extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label
+        data-testid="image-input-label"
+        htmlFor="image-input"
+      >
+        Imagem
+        <input
+          data-testid="image-input"
+          name="imagePath"
+          type="text"
+          value={ value }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+ImagePath.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
 export default ImagePath;
