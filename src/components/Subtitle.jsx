096d92fe2890c1153662e658f675f4f7d3331f35
jepaur5 <jepaur5@gmail.com>
2021-04-18 03:01:24 -0300

    implementando Subtitulo.jsx

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Subtitle extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitle">
+        Subtítulo
+        <input
+          data-testid="subtitle-input"
+          type="text"
+          name="subtitle"
+          value={ value }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+Subtitle.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
 export default Subtitle;
