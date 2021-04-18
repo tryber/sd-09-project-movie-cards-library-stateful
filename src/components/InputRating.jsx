25867f7a409092948ce64337dc3db8b4658c1157
jepaur5 <jepaur5@gmail.com>
2021-04-18 03:54:14 -0300

    implementando selecionaItem.jsx e ratingInput.jsx

--- src/components/InputRating.jsx
+++ src/components/InputRating.jsx
@@ -1,1 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class InputRating extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label
+        data-testid="rating-input-label"
+        htmlFor="rating-input"
+      >
+        Avaliação
+        <input
+          data-testid="rating-input"
+          name="rating"
+          type="number"
+          value={ value }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+InputRating.propTypes = {
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
 export default InputRating;
