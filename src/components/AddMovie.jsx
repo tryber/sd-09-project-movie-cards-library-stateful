05a315033a15c4ea15207a614d4f7a82fda395e4
FredericoCanoeira <fcanoeira@gmail.com>
2021-04-27 01:28:36 +0100

    resolve requisito 10,11 e  12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,38 +1,54 @@
 // implement AddMovie component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import TextArea from './CreateTextArea';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
   render() {
-    const { onClick } = this.props;
-
     return (
       <form data-testid="add-movie-form">
-        Título
         <label htmlFor="titleAdd" data-testid="title-input-label">
+        Título
           <input
             type="text"
             id="titleAdd"
             data-testid="title-input"
           />
         </label>
-        Subtítulo
         <label data-testid="subtitle-input-label" htmlFor="subt">
+        Subtítulo
           <input
             id="subt"
             data-testid="subtitle-input"
             type="text"
           />
         </label>
-        {/\* Treho abaix dando erro... \*/ }
-        Imagem
         <label data-testid="image-input-label" htmlFor="img">
+        Imagem
           <input
             type="text"
             id="img"
-            data-testid="subtitle-input"
+            data-testid="image-input"
           />
         </label>
+        <TextArea value={ storyline } onChange={ this.handleChange } />
       </form>
     );
   }
---
+++ src/components/CreateTextArea.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class TextArea extends React.Component{
+  render() {
+    const { value, onChange } = this.props;
+    return(
+      <label htmlFor="storyline" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            data-testid="storyline-input"
+            id="storyline"
+            value={ value }
+            onChange={ onChange }
+          />
+      </label>
+
+    );
+  }
+}
+
+export default TextArea;
