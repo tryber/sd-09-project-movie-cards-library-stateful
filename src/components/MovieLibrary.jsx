d85db8de33b4b223bc02c3daa8aaab23b2118a36
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2021-04-26 22:14:25 -0300

    searchbar 01

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+mport React, { Component } from 'react';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class searchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
+      return (
+        
+      )
+    }
+  }
